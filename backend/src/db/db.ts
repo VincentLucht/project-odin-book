11be7e190f24b24dafa932fdff68526d0e09538f
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-01-04 12:13:42 +0100

    Add sub-classes

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -2,16 +2,25 @@ import { PrismaClient } from '@prisma/client/default';
 
 import UserManager from '@/db/managers/userManager';
 import CommunityManager from '@/db/managers/communityManager';
+import TopicManager from '@/db/managers/communityTopicManager';
+import UserCommunityManager from '@/db/managers/userCommunityManager';
+import BannedUsersManager from '@/db/managers/bannedUsersManager';
 
 export class DB {
   private prisma: PrismaClient;
   public user: UserManager;
   public community: CommunityManager;
+  public userCommunity: UserCommunityManager;
+  public bannedUsers: BannedUsersManager;
+  public topic: TopicManager;
 
   constructor() {
     this.prisma = new PrismaClient();
     this.user = new UserManager(this.prisma);
     this.community = new CommunityManager(this.prisma);
+    this.userCommunity = new UserCommunityManager(this.prisma);
+    this.bannedUsers = new BannedUsersManager(this.prisma);
+    this.topic = new TopicManager(this.prisma);
   }
 }
 
