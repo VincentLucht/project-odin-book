72511e93cd107ecd53e81a87fb814577b9229d7c
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-01-04 12:21:11 +0100

    Add new checkers

--- backend/src/util/checker/checker.ts
+++ backend/src/util/checker/checker.ts
@@ -1,16 +1,25 @@
 import db, { DB } from '@/db/db';
 import UserChecker from '@/util/checker/parts/userChecker';
+import CommunityChecker from '@/util/checker/parts/communityChecker';
+import UserCommunityChecker from '@/util/checker/parts/userCommunityChecker';
+import BannedUsersChecker from '@/util/checker/parts/bannedUsersChecker';
 
 /**
  * Wrapper Class that checks frequent DB queries, like an user not being found and responds with the corresponding return status and message
  */
-class Checker {
+export class Checker {
   private db: DB;
   public user: UserChecker;
+  public community: CommunityChecker;
+  public userCommunity: UserCommunityChecker;
+  public bannedUsers: BannedUsersChecker;
 
   constructor() {
     this.db = db;
     this.user = new UserChecker(this.db);
+    this.community = new CommunityChecker(this.db);
+    this.userCommunity = new UserCommunityChecker(this.db);
+    this.bannedUsers = new BannedUsersChecker(this.db);
   }
 }
 
