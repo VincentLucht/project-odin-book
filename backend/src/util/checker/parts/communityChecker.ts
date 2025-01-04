f934149fac1f5e0c8db4e65cf7ec51a601caef62
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-01-04 12:21:38 +0100

    Add new checks

--- backend/src/util/checker/parts/communityChecker.ts
+++ backend/src/util/checker/parts/communityChecker.ts
@@ -1,7 +1,36 @@
 import { DB } from '@/db/db';
+import { Response } from 'express';
 
-export class CommunityChecker {
+export default class CommunityChecker {
   constructor(private db: DB) {}
 
-  async doesExist(id: string) {}
+  async notFoundById(res: Response, id: string) {
+    const community = await this.db.community.foundById(id);
+    if (!community) {
+      res.status(404).json({ message: 'Community does not exist' });
+      return true;
+    }
+
+    return false;
+  }
+
+  async foundByName(res: Response, name: string) {
+    const community = await this.db.community.notFoundByName(name);
+    if (community) {
+      res.status(409).json({ message: 'Community name is already in use' });
+      return true;
+    }
+
+    return false;
+  }
+
+  async isPrivate(res: Response, community_id: string) {
+    const isPrivate = await this.db.community.isPrivate(community_id);
+    if (isPrivate) {
+      res.status(400).json({ message: 'Can not join private community' });
+      return true;
+    }
+
+    return false;
+  }
 }
