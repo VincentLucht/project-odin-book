c7f0e4dd8ad53d071ea783b737323042a557ee83
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-01-04 12:21:57 +0100

    Add basic mock community

--- backend/src/util/test/testUtil.ts
+++ backend/src/util/test/testUtil.ts
@@ -1,3 +1,4 @@
+import { CommunityType } from '@prisma/client/default';
 import jwt from 'jsonwebtoken';
 
 export const mockUser = {
@@ -9,14 +10,30 @@ export const mockUser = {
   profile_picture_url: 't1',
   description: 't1',
   cake_day: 't1',
-  created_at: Date.now(),
+  created_at: new Date(),
 };
 
-export const generateToken = (userId: string, name: string) => {
+export const mockCommunity = {
+  id: '1',
+  name: 'mc',
+  description: 'desc',
+  profile_picture_url_desktop: 'https://example.com/profile-pic-desktop.jpg',
+  profile_picture_url_mobile: 'https://example.com/profile-pic-mobile.jpg',
+  banner_url: 'https://example.com/banner.jpg',
+  is_mature: false,
+  created_at: new Date(),
+  owner_id: 't1',
+  type: CommunityType.PUBLIC,
+  topics: ['topic1', 'topic2'],
+};
+
+export const generateToken = (userId: string, username: string) => {
   const secretKey = process.env.SECRET_KEY;
   if (!secretKey) {
     throw new Error('JWT_SECRET is not defined in the environment variables');
   }
 
-  return jwt.sign({ id: userId, name }, secretKey, { expiresIn: '5m' });
+  return jwt.sign({ id: userId, username }, secretKey, {
+    expiresIn: '5m',
+  });
 };
