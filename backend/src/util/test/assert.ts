1d31b88c85b9d3654899fb1ff27592f5dda74b5d
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-01-04 12:22:31 +0100

    Add class for writing asserts in tests faster

---
+++ backend/src/util/test/assert.ts
@@ -0,0 +1,39 @@
+class Assert {
+  exp(res: any, status: number, message: string) {
+    expect(res.status).toBe(status);
+    expect(res.body.message).toBe(message);
+  }
+
+  dbError(res: any) {
+    expect(res.status).toBe(500);
+    expect(res.body.message).toBe('Database error');
+  }
+
+  user = {
+    found: (res: any) => {
+      expect(res.status).toBe(409);
+      expect(res.body.message).toBe('User already exists');
+    },
+    notFound: (res: any) => {
+      expect(res.status).toBe(404);
+      expect(res.body.message).toBe('User does not exist');
+    },
+  };
+
+  community = {
+    notFound: (res: any) => {
+      expect(res.status).toBe(404);
+      expect(res.body.message).toBe('Community does not exist');
+    },
+  };
+
+  userCommunity = {
+    isMember: (res: any) => {
+      expect(res.status).toBe(409);
+      expect(res.body.message).toBe('You already joined that community');
+    },
+  };
+}
+
+const assert = new Assert();
+export default assert;
