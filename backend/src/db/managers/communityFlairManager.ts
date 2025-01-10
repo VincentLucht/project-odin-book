61cf2cb40993a79180a5e897e46b6137a8a3db53
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-01-10 11:20:39 +0100

    Rename method to match patterns, add method for ID

--- backend/src/db/managers/communityFlairManager.ts
+++ backend/src/db/managers/communityFlairManager.ts
@@ -3,11 +3,19 @@ import { PrismaClient } from '@prisma/client/default';
 export default class CommunityFlairManager {
   constructor(private prisma: PrismaClient) {}
 
-  async doesExist(community_id: string, name: string) {
+  async doesExistByName(community_id: string, name: string) {
     const count = await this.prisma.communityFlair.count({
       where: { community_id, name },
     });
     return count > 0;
+  }
+
+  async getById(community_id: string, community_flair_id: string) {
+    const flair = await this.prisma.communityFlair.findUnique({
+      where: { community_id, id: community_flair_id },
+    });
+
+    return flair;
   }
 
   async create(
