9399d86605d9893e436d70442192ed831185ccd2
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-01-20 14:18:24 +0100

    Add logo

---
+++ frontend/src/Header/components/Logo/Logo.tsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+  return (
+    <div className="gap-[6px] df">
+      <img src="/logo.png" alt="reddnir logo" className="max-h-[40px] min-w-[40px]" />
+      <h1 className="text-3xl font-bold">reddnir</h1>
+    </div>
+  );
+}
