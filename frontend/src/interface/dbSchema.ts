6347bbbc240f1ed01862f029172fd0426c8b8517
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-01-20 14:22:51 +0100

    Add db user schema

---
+++ frontend/src/interface/dbSchema.ts
@@ -0,0 +1,11 @@
+export interface DBUser {
+  id: string;
+  username: string;
+  email: string;
+  password: string;
+  display_name: string | null;
+  profile_picture_url: string | null;
+  description: string | null;
+  cake_day: string | null;
+  created_at: Date;
+}
