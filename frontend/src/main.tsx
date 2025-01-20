361d223a7e2edca9590e3989b236e43a993fd79b
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-01-20 14:11:31 +0100

    Add contexts and toast container

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '@/App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './tailwind.css';
+
+import App from '@/App';
+import Login from '@/auth/Login/Login';
+import SignUp from '@/auth/SignUp/SignUp';
+
+import ScreenSizeProvider from '@/context/screen/ScreenSizeProvider';
+import AuthProvider from '@/context/auth/AuthProvider';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <ScreenSizeProvider>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/sign-up" element={<SignUp />} />
+          </Routes>
+
+          <ToastContainer theme="dark" />
+        </ScreenSizeProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
