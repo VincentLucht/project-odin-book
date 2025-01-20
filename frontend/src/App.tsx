7d779f4b323bbd8fdca56668902b284adf952c0a
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-01-20 14:14:20 +0100

    Add components and search state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,21 @@
+import { useState } from 'react';
+
+import Header from '@/Header/Header';
+import Main from '@/Main/Main';
+import Sidebar from '@/SideBar/Sidebar';
+
 export default function App() {
-  return <div className="">I&apos;m a template</div>;
+  const [search, setSearch] = useState('');
+
+  return (
+    <div className="mx-auto flex h-dvh flex-col">
+      <Header search={search} setSearch={setSearch} />
+
+      <div className="grid flex-1 grid-cols-[270px_auto]">
+        <Sidebar />
+
+        <Main />
+      </div>
+    </div>
+  );
 }
