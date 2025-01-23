f080aa815aadbd4aaef0fdb3205f39ec83a37024
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-01-23 14:55:15 +0100

    Add components, remove border

--- frontend/src/Header/Header.tsx
+++ frontend/src/Header/Header.tsx
@@ -1,6 +1,13 @@
+import { useState } from 'react';
+import useAuth from '@/context/auth/hook/useAuth';
+
 import Logo from '@/Header/components/Logo/Logo';
 import InputWithImg from '@/components/InputWithImg';
+
+import CreateButton from '@/Header/components/CreateButton/CreateButton';
+import NotificationButton from '@/Header/components/NotificationButton/NotificationButton';
 import UserButton from '@/Header/components/UserButton/UserButton';
+import ChatButton from '@/Header/components/ChatButton/ChatButton';
 
 interface HeaderProps {
   search: string;
@@ -8,8 +15,11 @@ interface HeaderProps {
 }
 
 export default function Header({ search, setSearch }: HeaderProps) {
+  const [showDropDown, setShowDropDown] = useState<string | null>(null);
+  const { isLoggedIn } = useAuth();
+
   return (
-    <header className="grid max-h-[56px] min-h-[56px] grid-cols-[30%_40%_30%] border border-b-0 px-4 py-2">
+    <header className="grid max-h-[56px] min-h-[56px] grid-cols-[30%_40%_30%] border-b px-4 py-2">
       <div className="flex justify-start">
         <Logo />
       </div>
@@ -20,10 +30,25 @@ export default function Header({ search, setSearch }: HeaderProps) {
         src="/magnify.svg"
         alt="magnifying glass"
         placeholder="Search Reddnir"
+        className="bg-accent-gray"
       />
 
-      <div className="flex justify-end">
-        <UserButton />
+      <div className="flex justify-end gap-1">
+        {isLoggedIn && (
+          <>
+            <ChatButton />
+
+            <CreateButton />
+
+            <NotificationButton />
+          </>
+        )}
+
+        <UserButton
+          showDropDown={showDropDown === 'dropdown1'}
+          setShowDropDown={setShowDropDown}
+          dropdownId="dropdown1"
+        />
       </div>
     </header>
   );
