73f084072d9ce9c922064e121e01813bcd11f8ff
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-01-23 14:56:08 +0100

    Add DropDown menu

--- frontend/src/Header/components/UserButton/UserButton.tsx
+++ frontend/src/Header/components/UserButton/UserButton.tsx
@@ -1,14 +1,47 @@
 import useAuth from '@/context/auth/hook/useAuth';
 
 import LoginButton from '@/Header/components/LoginButton/LoginButton';
-import DropDownMenu from '@/components/DropDownMenu';
+import UserPFP from '@/components/user/UserPFP';
+import DropDownMenu from '@/components/DropDownMenu/DropDownMenu';
+import useClickOutside from '@/hooks/useClickOutside';
 
-export default function UserButton() {
+interface UserButtonProps {
+  showDropDown: boolean;
+  setShowDropDown: React.Dispatch<React.SetStateAction<string | null>>;
+  dropdownId: string;
+}
+
+export default function UserButton({
+  showDropDown,
+  setShowDropDown,
+  dropdownId,
+}: UserButtonProps) {
   const { isLoggedIn, user } = useAuth();
+
+  const dropdownRef = useClickOutside(() => {
+    setShowDropDown(null);
+  });
 
   if (!isLoggedIn) {
     return <LoginButton />;
   }
 
-  return <div>User Img</div>;
+  return (
+    <div className="df" ref={dropdownRef}>
+      <div>
+        <UserPFP
+          url={user.profile_picture_url}
+          onClick={() =>
+            setShowDropDown((prev) => (prev === dropdownId ? null : dropdownId))
+          }
+        />
+      </div>
+
+      <DropDownMenu
+        setterFunc={setShowDropDown}
+        className={`bg-hover-transition right-[20px] min-w-[256px] rounded-md df
+          ${showDropDown ? 'opacity-100' : 'opacity-0'} `}
+      />
+    </div>
+  );
 }
