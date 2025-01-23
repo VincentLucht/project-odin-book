7a01a46f26a05dc6514b871d861877271181317b
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-01-23 14:50:20 +0100

    Assert that if isLoggedIn exists, user exists

--- frontend/src/context/auth/hook/useAuth.ts
+++ frontend/src/context/auth/hook/useAuth.ts
@@ -1,10 +1,29 @@
 import { useContext } from 'react';
 import { AuthContext } from '@/context/auth/AuthProvider';
+import { TokenUser } from '@/context/auth/AuthProvider';
+
+interface AuthenticatedUser {
+  isLoggedIn: true;
+  user: TokenUser;
+  token: string;
+  login: (newToken: string) => void;
+  logout: () => void;
+}
+
+interface UnauthenticatedUser {
+  isLoggedIn: false;
+  user: null;
+  token: null;
+  login: (newToken: string) => void;
+  logout: () => void;
+}
+
+type Auth = AuthenticatedUser | UnauthenticatedUser;
 
 export default function useAuth() {
   const context = useContext(AuthContext);
   if (!context) {
     throw new Error('useAuth must be used within an AuthProvider');
   }
-  return context;
+  return context as Auth;
 }
