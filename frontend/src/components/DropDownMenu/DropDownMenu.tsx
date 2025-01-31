681cb79ea198eab63a4d581ae2532dee092a84f6
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-01-31 17:48:22 +0100

    Make Dropdown menu dynamic

--- frontend/src/components/DropDownMenu/DropDownMenu.tsx
+++ frontend/src/components/DropDownMenu/DropDownMenu.tsx
@@ -1,40 +1,20 @@
+import { ReactElement } from 'react';
 import useAuth from '@/context/auth/hook/useAuth';
 
-import DropDownButton from '@/components/DropDownMenu/components/DropDownButton';
-
-interface DropDownMenuProps {
-  setterFunc: React.Dispatch<React.SetStateAction<string | null>>;
+interface DropdownMenuProps {
+  children: ReactElement | ReactElement[];
   className?: string;
 }
 
-export default function DropDownMenu({ setterFunc, className }: DropDownMenuProps) {
+export default function DropdownMenu({ children, className }: DropdownMenuProps) {
   const { user } = useAuth();
 
-  if (!user) {
-    return;
-  }
+  if (!user) return;
 
   return (
     // TODO: Add box shadow
-    <div className={`bg-accent-gray absolute top-[58px] flex-col py-2 df ${className}`}>
-      <DropDownButton
-        text="View Profile"
-        subText={user.username}
-        src={`${user.profile_picture_url}`}
-        alt="View Profile"
-        route={`/user/${user.username}`}
-        size="large"
-        imgClassName="rounded-full border h-[32px] w-[32px]"
-        setterFunc={setterFunc}
-      />
-
-      <DropDownButton
-        text="Settings"
-        src="/cog-outline.svg"
-        alt="Settings"
-        route="/user/settings"
-        setterFunc={setterFunc}
-      />
+    <div className={`absolute top-[58px] flex-col py-2 df bg-accent-gray ${className}`}>
+      {children}
     </div>
   );
 }
