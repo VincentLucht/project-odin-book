c813b407e65d54e512469a342c01cc060b9c57b8
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-01-31 17:48:35 +0100

    Add styling and more props

--- frontend/src/components/DropDownMenu/components/DropDownButton.tsx
+++ frontend/src/components/DropDownMenu/components/DropDownButton.tsx
@@ -1,45 +1,63 @@
 import { useNavigate } from 'react-router-dom';
 
-interface DropDownButtonProps {
+interface DropdownButtonProps {
   text: string;
-  src: string;
-  alt: string;
-  route: string;
-  setterFunc: React.Dispatch<React.SetStateAction<string | null>>;
+  icon?: React.ReactNode;
+  src?: string;
+  alt?: string;
+  route?: string;
+  show: boolean;
+  setterFunc?: React.Dispatch<React.SetStateAction<string | null>>;
+  customFunc?: () => void;
   size?: 'normal' | 'large';
   subText?: string;
   imgClassName?: string;
 }
 
-export default function DropDownButton({
+export default function DropdownButton({
   text,
+  icon,
   src,
   alt,
   route,
+  show,
   setterFunc,
+  customFunc,
   size = 'normal',
   subText,
   imgClassName,
-}: DropDownButtonProps) {
+}: DropdownButtonProps) {
   const navigate = useNavigate();
+
+  console.log(src);
 
   return (
     <button
-      className={`text-mds dropdown-btn-transition flex w-full items-center gap-2 rounded-md px-4 py-2
-        font-light ${size === 'normal' ? 'h-[48px]' : 'h-[64px]'}`}
+      className={`text-mds flex w-full items-center gap-2 rounded-md px-4 py-2 font-light
+        dropdown-btn-transition ${size === 'normal' ? 'h-[48px]' : 'h-[64px]'}
+        ${show ? 'cursor-pointer' : 'cursor-default'}`}
       onClick={() => {
-        setterFunc(null);
-        navigate(route);
+        if (show && route && setterFunc) {
+          setterFunc(null);
+          navigate(route);
+        }
+        if (customFunc) {
+          customFunc();
+        }
       }}
     >
-      <div className="h-[32px] w-[32px] df">
-        <img src={src} alt={alt} className={`${imgClassName} h-6 w-6`} />
-      </div>
+      {icon ? (
+        <div className="h-[32px] w-[32px] df">{icon}</div>
+      ) : (
+        <div className="h-[32px] w-[32px] df">
+          <img src={src} alt={alt} className={`${imgClassName} h-6 w-6`} />
+        </div>
+      )}
 
       {subText ? (
         <div>
           <div className="-mt-[2px] mb-[2px] leading-tight">{text}</div>
-          <div className="text-gray-secondary text-start text-xs leading-none">
+          <div className="text-start text-xs leading-none text-gray-secondary">
             {subText}
           </div>
         </div>
