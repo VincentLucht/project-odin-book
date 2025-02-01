7b1851daff173af96078e86b48d53629b241d5c8
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-02-01 12:11:49 +0100

    Add hook for checking if user clicked outside

---
+++ frontend/src/hooks/useClickOutside.ts
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from 'react';
+
+export default function useClickOutside(closeFunction: () => void) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        closeFunction();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [closeFunction]);
+
+  return ref;
+}
