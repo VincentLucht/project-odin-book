c12a923452d7434de11e1ad39a892fd5d7a417a3
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-02-07 14:37:43 +0100

    Add functionality for Post.tsx voting

--- frontend/src/Main/Post/api/vote/handlePostVote.ts
+++ frontend/src/Main/Post/api/vote/handlePostVote.ts
@@ -1,56 +1,77 @@
-import { VoteType } from '@/interface/backendTypes';
 import deletePostVote from '@/Main/Post/api/vote/deletePostVote';
 import postVote from '@/Main/Post/api/vote/postVote';
 import catchError from '@/util/catchError';
+import getNewScore from '@/util/getNewScore';
+
+import { VoteType } from '@/interface/backendTypes';
 import { UserAndHistory } from '@/Main/user/UserProfile/api/fetchUserProfile';
+import { DBPostWithCommunity } from '@/interface/dbSchema';
 
 export default async function handlePostVote(
   post_id: string,
   userId: string,
   token: string,
   voteType: VoteType,
-  setFetchedUser: React.Dispatch<React.SetStateAction<UserAndHistory | null>>,
-  previousVoteType: VoteType | null,
+  setterFunc: React.Dispatch<
+    React.SetStateAction<UserAndHistory | DBPostWithCommunity | null>
+  >,
+  previousVoteType: VoteType | undefined,
 ) {
-  const getNewScore = (currentScore: number) => {
-    if (previousVoteType === voteType) {
-      // Removing vote
-      return previousVoteType === 'UPVOTE' ? currentScore - 1 : currentScore + 1;
-    } else {
-      // Adding/Changing vote
-      if (previousVoteType) {
-        return voteType === 'UPVOTE' ? currentScore + 2 : currentScore - 2;
-      } else {
-        return voteType === 'UPVOTE' ? currentScore + 1 : currentScore - 1;
-      }
-    }
-  };
-
-  let previousState: UserAndHistory | null = null;
-
-  // Optimistic update
-  setFetchedUser((prev) => {
+  const updateFetchedUserState = (
+    prev: UserAndHistory | DBPostWithCommunity | null,
+    post_id: string,
+    previousVoteType: VoteType | undefined,
+    voteType: VoteType,
+    userId: string,
+  ) => {
     if (!prev) return prev;
     previousState = prev;
 
-    return {
-      ...prev,
-      history: prev.history.map((value) => {
-        if (value.id === post_id) {
-          return {
-            ...value,
-            post_votes:
-              previousVoteType === voteType
-                ? []
-                : [{ user_id: userId, vote_type: voteType }],
-            total_vote_score: getNewScore(value.total_vote_score),
-          };
-        }
+    // UserAndHistory
+    if ('history' in prev) {
+      return {
+        ...prev,
+        history: prev.history.map((value) => {
+          if (value.id === post_id) {
+            return {
+              ...value,
+              post_votes:
+                previousVoteType === voteType
+                  ? []
+                  : [{ user_id: userId, vote_type: voteType }],
+              total_vote_score: getNewScore(
+                value.total_vote_score,
+                voteType,
+                previousVoteType,
+              ),
+            };
+          }
+          return value;
+        }),
+      };
+    } else {
+      // DDPostWithCommunity
+      return {
+        ...prev,
+        post_votes:
+          previousVoteType === voteType
+            ? []
+            : [{ user_id: userId, vote_type: voteType }],
+        total_vote_score: getNewScore(
+          prev.total_vote_score,
+          voteType,
+          previousVoteType,
+        ),
+      };
+    }
+  };
 
-        return value;
-      }),
-    };
-  });
+  let previousState: UserAndHistory | DBPostWithCommunity | null = null;
+
+  // Optimistic update
+  setterFunc((prev) =>
+    updateFetchedUserState(prev, post_id, previousVoteType, voteType, userId),
+  );
 
   try {
     // Perform actual API call
@@ -61,8 +82,21 @@ export default async function handlePostVote(
     }
   } catch (error) {
     if (previousState) {
-      setFetchedUser(previousState);
+      setterFunc(previousState);
     }
-    catchError(error);
+
+    // allow to change frontend object if frontend obj is out of sync
+    const errorObj = error as { message?: string };
+    if (
+      ('message' in errorObj &&
+        errorObj.message === 'You already voted for this post') ||
+      errorObj.message === 'Vote not found'
+    ) {
+      setterFunc((prev) =>
+        updateFetchedUserState(prev, post_id, previousVoteType, voteType, userId),
+      );
+    } else {
+      catchError(error);
+    }
   }
 }
