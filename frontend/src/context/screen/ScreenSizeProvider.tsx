b5a9d254bb5e1a09ab726fb5fff32c02baa56a03
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-02-10 14:17:12 +0100

    Add additional isDesktop bool for easier conditionals

--- frontend/src/context/screen/ScreenSizeProvider.tsx
+++ frontend/src/context/screen/ScreenSizeProvider.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect, createContext } from 'react';
 export const ScreenSizeContext = createContext({
   isMobile: false,
   isSmallScreen: false,
+  isDesktop: true,
 });
 
 export default function ScreenSizeProvider({ children }: { children: any }) {
@@ -10,12 +11,18 @@ export default function ScreenSizeProvider({ children }: { children: any }) {
   const [isSmallScreen, setIsSmallScreen] = useState(
     window.innerWidth >= 768 && window.innerWidth < 1280,
   );
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 1280);
 
   useEffect(() => {
     const handleResize = () => {
       const currentWidth = window.innerWidth;
-      setIsMobile(currentWidth < 768);
-      setIsSmallScreen(currentWidth >= 768 && currentWidth < 1280);
+      const mobile = currentWidth < 768;
+      const smallScreen = currentWidth >= 768 && currentWidth < 1280;
+      const desktop = currentWidth >= 1280;
+
+      setIsMobile(mobile);
+      setIsSmallScreen(smallScreen);
+      setIsDesktop(desktop);
     };
 
     window.addEventListener('resize', handleResize);
@@ -23,7 +30,7 @@ export default function ScreenSizeProvider({ children }: { children: any }) {
   }, []);
 
   return (
-    <ScreenSizeContext.Provider value={{ isMobile, isSmallScreen }}>
+    <ScreenSizeContext.Provider value={{ isMobile, isSmallScreen, isDesktop }}>
       {children}
     </ScreenSizeContext.Provider>
   );
