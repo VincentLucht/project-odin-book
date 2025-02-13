9680f8e4a4c7f3d0d02df35f4889a0dd98715467
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-02-13 13:26:30 +0100

    Add replying to comments

--- frontend/src/Main/Post/components/CommentSection/components/Comments/components/Comment/Comment.tsx
+++ frontend/src/Main/Post/components/CommentSection/components/Comments/components/Comment/Comment.tsx
@@ -1,41 +1,88 @@
+import { useState } from 'react';
+
 import UserPFP from '@/components/user/UserPFP';
-import CommentInteractionBar from '@/Main/Post/components/CommentSection/components/Comments/components/CommentInteractionBar/CommentInteractionBar';
+import CommentInteractionBar from '@/Main/Post/components/CommentSection/components/Comments/components/Comment/components/CommentInteractionBar';
+import HideOrShow from '@/Main/Post/components/CommentSection/components/Comments/components/Comment/components/HideOrShow';
+import ReplyEditor from '@/Main/Post/components/ReplyEditor/ReplyEditor';
+import MoreRepliesButton from '@/Main/Post/components/CommentSection/components/Comments/components/Comment/components/MoreRepliesButton/MoreRepliesButton';
+
 import getRelativeTime from '@/util/getRelativeTime';
 
 import { DBCommentWithReplies } from '@/interface/dbSchema';
+import { DBPostWithCommunity } from '@/interface/dbSchema';
 import { NavigateFunction } from 'react-router-dom';
 import { VoteType } from '@/interface/backendTypes';
+import { TokenUser } from '@/context/auth/AuthProvider';
 
 interface CommentProps {
   comment: DBCommentWithReplies;
   depth: number;
-  userId: string | undefined;
+  user: TokenUser | null;
+  token: string | null;
+  postId: string;
   navigate: NavigateFunction;
   onVote: (
     commentId: string,
     voteType: VoteType,
     previousVoteType: VoteType | undefined,
   ) => void;
+  setComments: React.Dispatch<React.SetStateAction<DBCommentWithReplies[] | null>>;
+  setPost: React.Dispatch<React.SetStateAction<DBPostWithCommunity | null>>;
 }
 
 // TODO: Mark post owner comments as OP
 // TODO: Add user flair :)
+// TODO: Add editing comments if you are the creator
+// TODO: Add share/copy functionality
+// TODO: Add save functionality for comment
+// TODO: Add hiding comments
 export default function Comment({
   comment,
   depth,
-  userId,
+  user,
+  token,
+  postId,
   navigate,
   onVote,
+  setComments,
+  setPost,
 }: CommentProps) {
+  const [showReply, setShowReply] = useState(false);
+  const [commentText, setCommentText] = useState('');
+  const [hide, setHide] = useState(false);
+
+  const hasReply = comment.replies === undefined && comment._count.replies >= 1;
+
   const redirectToUser = (username: string) => {
     navigate(`/user/${username}`);
+  };
+
+  const toggleShow = (wasSubmitted = false) => {
+    if (wasSubmitted) {
+      setCommentText('');
+      setShowReply(false);
+      return;
+    }
+
+    if (commentText) {
+      const cancel = confirm(
+        'You have a comment in progress, are you sure you want to discard it?',
+      );
+
+      if (cancel) {
+        setCommentText('');
+        setShowReply(!showReply);
+      }
+    } else {
+      setShowReply(!showReply);
+    }
   };
 
   return (
     <>
       <div
-        className="my-4 flex flex-col gap-1"
-        style={{ marginLeft: `${depth * 40}px` }}
+        className="flex flex-col gap-1 transition-all"
+        style={{ marginLeft: `${depth * 30}px` }}
       >
         <div className="flex items-center gap-1">
           <UserPFP
@@ -53,32 +100,59 @@ export default function Comment({
         </div>
 
         <div className="flex">
-          <div className="min-w-11"></div>
+          <div className="grid min-w-11 grid-rows-[1fr_32px]">
+            <div className="-mb-[6px] -mt-[3px] df">
+              <div className="h-full max-w-[1px] border border-gray-500"></div>
+            </div>
+
+            <HideOrShow hide={hide} setHide={setHide} />
+          </div>
 
           <div>
-            <div>{comment.content}</div>
+            <div className="break-all text-[14.5px]">{comment.content}</div>
 
             <CommentInteractionBar
               totalVoteCount={comment.total_vote_score}
               userVote={{
-                hasVoted: comment?.comment_votes?.[0]?.user_id === userId,
+                hasVoted: comment?.comment_votes?.[0]?.user_id === user?.id,
                 voteType: comment?.comment_votes?.[0]?.vote_type,
               }}
               commentId={comment.id}
               onVoteComment={onVote}
+              toggleShow={toggleShow}
             />
           </div>
         </div>
+
+        <ReplyEditor
+          depth={0 + 1}
+          show={showReply}
+          toggleShow={toggleShow}
+          setCommentText={setCommentText}
+          commentText={commentText}
+          postId={postId}
+          parentCommentId={comment.id}
+          setComments={setComments}
+          setPost={setPost}
+          user={user}
+          token={token}
+        />
+
+        <MoreRepliesButton hasReply={hasReply} parent_comment_id={comment.id} />
       </div>
 
       {comment.replies?.map((commentReply) => (
         <Comment
           comment={commentReply}
           depth={depth + 1}
-          userId={userId}
+          user={user}
+          token={token}
+          postId={postId}
           navigate={navigate}
           key={commentReply.id}
           onVote={onVote}
+          setComments={setComments}
+          setPost={setPost}
         />
       ))}
     </>
