ea66cbeb87f19393817c792da1d104f530de5240
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-02-14 14:49:11 +0100

    Add function for creating a temporary reply

---
+++ frontend/src/Main/Post/components/ReplyEditor/api/post/util/createTempComment.ts
@@ -0,0 +1,32 @@
+import { DBCommentWithReplies } from '@/interface/dbSchema';
+
+export default function createTempComment(
+  content: string,
+  post_id: string,
+  user_id: string,
+  username: string,
+  profile_picture_url: string | undefined,
+  parent_comment_id: string | undefined,
+) {
+  const tempId = `temp-${Date.now()}-${Math.random().toString(36).substring(2, 10)}`;
+
+  const tempComment: DBCommentWithReplies = {
+    id: tempId,
+    content,
+    created_at: new Date(),
+    updated_at: new Date(),
+    is_deleted: false,
+    upvote_count: 1,
+    downvote_count: 0,
+    total_vote_score: 1,
+
+    post_id,
+    user_id,
+    user: { username, profile_picture_url: profile_picture_url ?? null },
+    replies: [],
+    comment_votes: [{ user_id, vote_type: 'UPVOTE' }],
+    parent_comment_id,
+  };
+
+  return tempComment;
+}
