6b976bdb965d7b4fbf17d05efb80d3a4f1511247
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-02-14 13:24:23 +0100

    Add logic for displaying "more replies" comment

--- frontend/src/Main/Post/components/CommentSection/CommentSection.tsx
+++ frontend/src/Main/Post/components/CommentSection/CommentSection.tsx
@@ -1,33 +1,74 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 import Comments from '@/Main/Post/components/CommentSection/components/Comments/Comments';
+import AddComment from '@/Main/Post/components/CommentSection/components/AddComment/AddComment';
 
 import handleFetchComments from '@/Main/Post/components/CommentSection/api/handleFetchComments';
+import handleFetchReplies from '@/Main/Post/components/CommentSection/components/Comments/components/Comment/components/MoreRepliesButton/api/handleFetchReplies';
+import getBaseURL from '@/Main/Post/components/CommentSection/util/getBaseURL';
 
 import { DBCommentWithReplies } from '@/interface/dbSchema';
+import { DBPostWithCommunity } from '@/interface/dbSchema';
+import { TokenUser } from '@/context/auth/AuthProvider';
 
 interface CommentSectionProps {
   postId: string;
-  userId: string | undefined;
+  user: TokenUser | null;
   token: string | null;
+  setPost: React.Dispatch<React.SetStateAction<DBPostWithCommunity | null>>;
 }
 
-export default function CommentSection({ postId, userId, token }: CommentSectionProps) {
+export default function CommentSection({
+  postId,
+  user,
+  token,
+  setPost,
+}: CommentSectionProps) {
   const [comments, setComments] = useState<DBCommentWithReplies[] | null>(null);
 
+  const { parentCommentId } = useParams();
+  const navigate = useNavigate();
+  const location = useLocation();
+
   useEffect(() => {
-    handleFetchComments(postId, token, setComments);
-  }, [postId, token]);
+    if (parentCommentId) {
+      handleFetchReplies(token, postId, parentCommentId, setComments);
+    } else {
+      handleFetchComments(postId, token, setComments);
+    }
+  }, [postId, token, parentCommentId]);
 
   return (
     <div className="pt-2">
-      <div>Write a comment</div>
+      <AddComment
+        postId={postId}
+        user={user}
+        token={token}
+        setComments={setComments}
+        setPost={setPost}
+      />
+
+      {parentCommentId && (
+        <div className="-mb-4 mt-4 df">
+          <button
+            className="text-sm text-blue-400 transition-all duration-1000 hover:underline"
+            onClick={() => navigate(getBaseURL(location.pathname))}
+          >
+            See full discussion
+          </button>
+        </div>
+      )}
 
       <Comments
         comments={comments}
-        userId={userId}
+        user={user}
         token={token}
+        postId={postId}
         setComments={setComments}
+        setPost={setPost}
       />
     </div>
   );
