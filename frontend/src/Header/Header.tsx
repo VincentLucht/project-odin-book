2c047ae6f7389d7f3c9744afe8c5be50276979f6
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-02-14 13:19:11 +0100

    Add toggle for Sidebar on smaller screens

--- frontend/src/Header/Header.tsx
+++ frontend/src/Header/Header.tsx
@@ -3,23 +3,46 @@ import useAuth from '@/context/auth/hook/useAuth';
 
 import Logo from '@/Header/components/Logo/Logo';
 import InputWithImg from '@/components/InputWithImg';
-
 import CreateButton from '@/Header/components/CreateButton/CreateButton';
 import NotificationButton from '@/Header/components/NotificationButton/NotificationButton';
 import UserButton from '@/Header/components/UserButton/UserButton';
 import ChatButton from '@/Header/components/ChatButton/ChatButton';
+import { MenuIcon } from 'lucide-react';
 
 interface HeaderProps {
   search: string;
   setSearch: React.Dispatch<React.SetStateAction<string>>;
+  isDesktop: boolean;
+  setShowSidebar: React.Dispatch<React.SetStateAction<boolean>>;
+  headerRef: React.RefObject<HTMLElement>;
 }
 
-export default function Header({ search, setSearch }: HeaderProps) {
+export default function Header({
+  search,
+  setSearch,
+  isDesktop,
+  setShowSidebar,
+  headerRef,
+}: HeaderProps) {
   const [showDropDown, setShowDropDown] = useState<string | null>(null);
   const { isLoggedIn } = useAuth();
 
   return (
-    <header className="grid max-h-[56px] min-h-[56px] grid-cols-[30%_40%_30%] border-b px-4 py-2">
+    <header
+      className={`grid max-h-[56px] min-h-[56px]
+        ${isDesktop ? 'grid-cols-[30%_40%_30%]' : 'grid-cols-[5%_25%_40%_30%]'} border-b px-4
+        py-2`}
+      ref={headerRef}
+    >
+      {!isDesktop && (
+        <div
+          className="bg-hover-transition"
+          onClick={() => setShowSidebar((prev) => !prev)}
+        >
+          <MenuIcon />
+        </div>
+      )}
+
       <div className="flex justify-start">
         <Logo />
       </div>
