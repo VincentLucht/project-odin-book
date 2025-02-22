033dc391bffa46518fbf8d83639442495736efd7
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-02-22 13:19:04 +0100

    Add post sidebar placeholder

---
+++ frontend/src/Main/Post/components/PostSidebar/PostSidebar.tsx
@@ -0,0 +1,51 @@
+import { ReactNode } from 'react';
+
+import formatDate from '@/util/formatDate';
+
+import { CommunityMembership } from '@/interface/dbSchema';
+import { CommunityTypes } from '@/interface/dbSchema';
+
+export interface CommunitySideBar {
+  id: string;
+  name: string;
+  description: string | null;
+  profile_picture_url: string | null;
+  created_at: string;
+  is_mature: boolean;
+  type: CommunityTypes;
+  user_communities: CommunityMembership[];
+}
+
+interface PostSidebarProps {
+  community: CommunitySideBar | null;
+}
+
+// TODO: Make it fixed
+// TODO: Add community type icon + created at
+export default function PostSidebar({ community }: PostSidebarProps) {
+  if (!community) {
+    return;
+  }
+
+  return (
+    <div>
+      <div className="flex flex-col gap-2 rounded-md bg-gray-700 p-4">
+        <div className="flex items-center justify-between">
+          <h3 className="gap-1 text-xl font-semibold text-hidden-ellipsis">
+            r/{community.name}
+          </h3>
+
+          <div>Is joined?</div>
+        </div>
+
+        <div>{community.description}</div>
+
+        <div>
+          <div>{formatDate(community.created_at)}</div>
+
+          <div>{community.type as ReactNode}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
