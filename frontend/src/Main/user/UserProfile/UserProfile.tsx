ee57e2b500037055775e4d201299cdc024dbb225
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-02-22 13:19:31 +0100

    Pass through props

--- frontend/src/Main/user/UserProfile/UserProfile.tsx
+++ frontend/src/Main/user/UserProfile/UserProfile.tsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
 import useAuthGuard from '@/context/auth/hook/useAuthGuard';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import fetchUserProfile, {
   UserAndHistory,
 } from '@/Main/user/UserProfile/api/fetchUserProfile';
 import isPost from '@/Main/user/UserProfile/util/isPost';
-import PostOverview from '@/Main/Post/PostOverview/PostOverview';
+import PostOverview from '@/Main/Post/components/PostOverview/PostOverview';
 import UserSideBar from '@/Main/user/UserProfile/components/UserSidebar/UserSidebar';
 import CommentOverview from '@/Main/CommentOverview/CommentOverview';
 import UserNotFound from '@/components/partials/UserNotFound';
@@ -14,13 +14,16 @@ import UserNotFound from '@/components/partials/UserNotFound';
 import { SortByUser } from '@/interface/backendTypes';
 import { toast } from 'react-toastify';
 
+// TODO: Make this work without being logged in!
 export default function UserProfile() {
   const [page, setPage] = useState(1);
   const [sortBy, setSortBy] = useState<SortByUser>('new');
   const [fetchedUser, setFetchedUser] = useState<UserAndHistory | null>(null);
+  const [showCommentDropdown, setShowCommentDropdown] = useState<string | null>(null);
 
   const { user, token } = useAuthGuard();
 
+  const navigate = useNavigate();
   const path = useParams();
   const { username } = path;
 
@@ -46,6 +49,7 @@ export default function UserProfile() {
         {fetchedUser ? (
           <div className="w-full min-w-0">
             <div className="flex gap-2">
+              {/* TODO: Img is a bit distorted?? use object-cover?? */}
               <img
                 src={`${fetchedUser?.profile_picture_url ? fetchedUser?.profile_picture_url : '/user.svg'}`}
                 alt="User Profile Pictures"
@@ -72,9 +76,24 @@ export default function UserProfile() {
                     userId={user.id}
                     token={token}
                     setFetchedUser={setFetchedUser}
+                    navigate={navigate}
                   />
                 ) : (
-                  <CommentOverview key={index} comment={value} />
+                  <CommentOverview
+                    key={index}
+                    urlItems={{
+                      communityName: value.post.community.name,
+                      postId: value.post_id,
+                      postName: value.post.title,
+                    }}
+                    comment={value}
+                    userId={user.id}
+                    token={token}
+                    showCommentDropdown={showCommentDropdown}
+                    setShowCommentDropdown={setShowCommentDropdown}
+                    setFetchedUser={setFetchedUser}
+                    navigate={navigate}
+                  />
                 ),
               )
             ) : (
