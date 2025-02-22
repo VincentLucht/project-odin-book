732066d32e38d5601d8857763dc061750f2f1002
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-02-22 13:14:00 +0100

    Add component

---
+++ frontend/src/Main/Post/components/PostContent/PostContent.tsx
@@ -0,0 +1,57 @@
+import { Transition } from '@headlessui/react';
+import PostEditor from '@/Main/Post/components/PostEditor/PostEditor';
+
+import transitionPropsHeight from '@/util/transitionProps';
+import { DBPostWithCommunity } from '@/interface/dbSchema';
+import { TrashIcon } from 'lucide-react';
+
+interface PostContentProps {
+  post: DBPostWithCommunity;
+  isEditActive: boolean;
+  setIsEditActive: React.Dispatch<React.SetStateAction<boolean>>;
+  setPost: React.Dispatch<React.SetStateAction<DBPostWithCommunity | null>>;
+  token: string | null;
+}
+
+export default function PostContent({
+  post,
+  isEditActive,
+  setIsEditActive,
+  setPost,
+  token,
+}: PostContentProps) {
+  const toggleShow = () => {
+    setIsEditActive((prev) => !prev);
+  };
+
+  return (
+    <div className="py-4 pt-2">
+      <Transition show={!isEditActive} {...transitionPropsHeight}>
+        <div>
+          {post.deleted_at ? (
+            <div className="flex items-center gap-2 rounded-md border border-gray-600 p-4 text-sm text-gray-400">
+              <TrashIcon className="flex-shrink-0 text-red-500" />
+              <span className="break-words">
+                Sorry, this post was deleted by the person who originally posted it
+              </span>
+            </div>
+          ) : (
+            <span className="py-4 pt-2">{post.body}</span>
+          )}
+        </div>
+      </Transition>
+
+      <Transition show={isEditActive} {...transitionPropsHeight}>
+        <div>
+          <PostEditor
+            post={post}
+            setPost={setPost}
+            toggleShow={toggleShow}
+            setIsEditActive={setIsEditActive}
+            token={token}
+          />
+        </div>
+      </Transition>
+    </div>
+  );
+}
