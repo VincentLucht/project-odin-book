c7755bec58c6426d36c309fd47bf47ecdc8c94ff
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-02-22 13:17:02 +0100

    Add util function for deleting

---
+++ frontend/src/util/confirmDelete.ts
@@ -0,0 +1,7 @@
+export default function confirmDelete(itemType: string) {
+  const willDelete = confirm(
+    `Delete ${itemType}?\n\nAre you sure you want to delete your ${itemType}? You can't undo this.`,
+  );
+
+  return willDelete;
+}
