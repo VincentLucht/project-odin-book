c109b3f2a24fc219f4f7ae76309cc0643e0aea25
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-02-22 12:34:41 +0100

    Add various functions for post editing: editing post, adding spoiler and NSFW tag

--- frontend/src/components/Interaction/Ellipsis.tsx
+++ frontend/src/components/Interaction/Ellipsis.tsx
@@ -3,25 +3,34 @@ import useClickOutside from '@/hooks/useClickOutside';
 import DropdownMenu from '@/components/DropdownMenu/DropdownMenu';
 import DropdownButton from '@/components/DropdownMenu/components/DropdownButton';
 import { EllipsisIcon, PencilIcon, BookmarkIcon, TrashIcon } from 'lucide-react';
+import { SquarePenIcon, TagIcon, CircleAlertIcon, BanIcon } from 'lucide-react';
 
 interface EllipsisProps {
   isUserSelf: boolean;
   commentId: string;
-  mode?: 'overview' | 'comment';
+  mode?: 'post' | 'comment';
   showDropdown: string | null;
   setShowDropdown: React.Dispatch<React.SetStateAction<string | null>>;
-  setIsEditActive: React.Dispatch<React.SetStateAction<boolean>>;
-  deleteFunc: () => void;
+  setIsEditActive?: React.Dispatch<React.SetStateAction<boolean>>;
+  deleteFunc: (commentId: string) => void;
+  postFlairFunc?: () => void;
+  spoilerFunc?: () => void;
+  matureFunc?: () => void;
+  editFunc?: () => void;
 }
 
 export default function Ellipsis({
   isUserSelf,
   commentId,
-  mode = 'overview',
+  mode = 'post',
   showDropdown,
   setShowDropdown,
   setIsEditActive,
   deleteFunc,
+  postFlairFunc,
+  spoilerFunc,
+  matureFunc,
+  editFunc,
 }: EllipsisProps) {
   const divRef = useClickOutside(() => {
     setShowDropdown(null);
@@ -30,14 +39,19 @@ export default function Ellipsis({
   return (
     <div className="relative" ref={divRef}>
       <div
-        className={`cursor-pointer px-3 transition-all hover:bg-hover-gray active:bg-active-gray
-          ${mode === 'overview' ? 'interaction-button-wrapper' : 'text-gray-400 interaction-button-wrapper-secondary hover:text-white'}`}
+        className={`cursor-pointer px-3 transition-all interaction-button-wrapper-secondary
+          hover:bg-hover-gray active:bg-active-gray ${ mode === 'comment' &&
+          'text-gray-400 hover:text-white' }`}
         onClick={() =>
           setShowDropdown((prev) => (prev === commentId ? null : commentId))
         }
         onMouseDown={(e) => e.stopPropagation()}
       >
-        <EllipsisIcon className={`${mode === 'overview' ? 'h-5 w-5' : 'h-4 w-4'}`} />
+        {mode === 'post' ? (
+          <SquarePenIcon className="h-5 w-5" />
+        ) : (
+          <EllipsisIcon className="h-[18px] w-[18px]" />
+        )}
       </div>
 
       <DropdownMenu
@@ -45,11 +59,17 @@ export default function Ellipsis({
           ${showDropdown === commentId ? '!z-10 opacity-100' : '!-z-10 opacity-0'} `}
       >
         {isUserSelf ? (
-          <div className="w-full" onClick={() => setIsEditActive(true)}>
+          <div
+            className="w-full"
+            onClick={() => {
+              setIsEditActive && setIsEditActive(true);
+              editFunc && editFunc();
+            }}
+          >
             <DropdownButton
-              text="Edit comment"
-              icon={mode === 'comment' && <PencilIcon className="h-[22px] w-[22px]" />}
-              alt="Edit comment"
+              text={`${mode === 'post' ? 'Edit post' : 'Edit comment'}`}
+              icon={<PencilIcon className="h-[22px] w-[22px]" />}
+              alt={`${mode === 'post' ? 'Edit post' : 'Edit comment'}`}
               imgClassName="rounded-full border h-[32px] w-[32px]"
               setterFunc={setShowDropdown}
               show={showDropdown === commentId}
@@ -60,20 +80,66 @@ export default function Ellipsis({
         )}
         {
           <DropdownButton
-            text="Save comment"
-            icon={mode === 'comment' && <BookmarkIcon />}
-            alt="Save comment"
+            text={`${mode === 'post' ? 'Save post' : 'Save comment'}`}
+            icon={<BookmarkIcon />}
+            alt={`${mode === 'post' ? 'Save post' : 'Save comment'}`}
             imgClassName="rounded-full border h-[32px] w-[32px]"
             setterFunc={setShowDropdown}
             show={showDropdown === commentId}
           />
         }
         {isUserSelf ? (
-          <div className="w-full" onClick={() => deleteFunc()}>
+          <div className="w-full" onClick={() => deleteFunc(commentId)}>
             <DropdownButton
-              text="Delete comment"
-              icon={mode === 'comment' && <TrashIcon />}
-              alt="Delete comment"
+              text={`${mode === 'post' ? 'Delete post' : 'Delete comment'}`}
+              icon={<TrashIcon />}
+              alt={`${mode === 'post' ? 'Delete post' : 'Delete comment'}`}
+              imgClassName="rounded-full border h-[32px] w-[32px]"
+              setterFunc={setShowDropdown}
+              show={showDropdown === commentId}
+            />
+          </div>
+        ) : (
+          <></>
+        )}
+
+        {/* POST ADDITIONS */}
+        {mode === 'post' && isUserSelf ? (
+          <div className="w-full" onClick={() => postFlairFunc && postFlairFunc()}>
+            <DropdownButton
+              text="Add Post Flair"
+              icon={<TagIcon />}
+              alt="Add Post Flair"
+              imgClassName="rounded-full border h-[32px] w-[32px]"
+              setterFunc={setShowDropdown}
+              show={showDropdown === commentId}
+            />
+          </div>
+        ) : (
+          <></>
+        )}
+
+        {mode === 'post' && isUserSelf ? (
+          <div className="w-full" onClick={() => matureFunc && matureFunc()}>
+            <DropdownButton
+              text="Add NSFW tag"
+              icon={<BanIcon />}
+              alt="Add NSFW tag"
+              imgClassName="rounded-full border h-[32px] w-[32px]"
+              setterFunc={setShowDropdown}
+              show={showDropdown === commentId}
+            />
+          </div>
+        ) : (
+          <></>
+        )}
+
+        {mode === 'post' && isUserSelf ? (
+          <div className="w-full" onClick={() => spoilerFunc && spoilerFunc()}>
+            <DropdownButton
+              text="Add spoiler tag"
+              icon={<CircleAlertIcon />}
+              alt="Add spoiler tag"
               imgClassName="rounded-full border h-[32px] w-[32px]"
               setterFunc={setShowDropdown}
               show={showDropdown === commentId}
