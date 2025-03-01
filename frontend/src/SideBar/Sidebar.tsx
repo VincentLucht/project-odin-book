757998c90cc512c2f37b78eaeb970007af0d2ea3
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-03-01 10:38:14 +0100

    Add recent and joined communities, and other buttons

--- frontend/src/SideBar/Sidebar.tsx
+++ frontend/src/SideBar/Sidebar.tsx
@@ -1,3 +1,56 @@
+import { useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import useAuth from '@/context/auth/hook/useAuth';
+import useSetActiveButton from '@/Sidebar/hooks/useSetActiveButton';
+
+import SidebarButton from '@/Sidebar/components/ui/SidebarButton';
+import Separator from '@/components/Separator';
+import SidebarNotLoggedIn from '@/Sidebar/components/SidebarNotLoggedIn/SidebarNotLoggedIn';
+import RecentCommunities from '@/Sidebar/components/RecentCommunities/RecentCommunities';
+import JoinedCommunities from '@/Sidebar/components/JoinedCommunities/JoinedCommunities';
+import Resources from '@/Sidebar/components/Resources/Resources';
+
 export default function Sidebar() {
-  return <nav className="border border-r-0">Sidebar</nav>;
+  const [activeButton, setActiveButton] = useState('');
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { user, token } = useAuth();
+  useSetActiveButton(location, setActiveButton);
+
+  if (!user) {
+    return <SidebarNotLoggedIn />;
+  }
+
+  return (
+    <nav className="h-full w-[270px] overflow-y-scroll border-r bg-gray">
+      <div className="flex-col pt-4 df">
+        <SidebarButton
+          navigate={() => navigate('')}
+          buttonName="Home"
+          src={activeButton === 'Home' ? '/home.svg' : '/home-outline.svg'}
+          alt="Home"
+        />
+
+        <SidebarButton
+          navigate={() => navigate('/popular')}
+          buttonName="Popular"
+          src={
+            activeButton === 'trending'
+              ? '/trending-up.svg'
+              : '/trending-up-outline.svg'
+          }
+          alt="Trending"
+        />
+
+        <Separator mode="sidebar" />
+        <RecentCommunities navigate={navigate} />
+
+        <Separator mode="sidebar" />
+        <JoinedCommunities navigate={navigate} token={token} />
+
+        <Separator mode="sidebar" />
+        <Resources navigate={navigate} />
+      </div>
+    </nav>
+  );
 }
