795829a1a56903d62b57b86f3e260d98e9f912e6
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-03-01 10:35:23 +0100

    Add additional mode

--- frontend/src/components/Separator.tsx
+++ frontend/src/components/Separator.tsx
@@ -1,3 +1,11 @@
-export default function Separator() {
-  return <hr className="h-[1px] border-0 bg-gray-600" />;
+interface SeparatorProps {
+  mode?: 'normal' | 'sidebar';
+}
+
+export default function Separator({ mode = 'normal' }: SeparatorProps) {
+  return (
+    <hr
+      className={`h-[1px] border-0 bg-gray-600 ${mode === 'sidebar' && 'my-3 w-[215px]'}`}
+    />
+  );
 }
