21c1495c662ca272c84087edd1e3c29429c5bf06
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-03-01 10:43:35 +0100

    Add recent community manager

---
+++ backend/src/db/managers/recentCommunitiesManager.ts
@@ -0,0 +1,62 @@
+import { PrismaClient } from '@prisma/client/default';
+
+export default class RecentCommunitiesManager {
+  constructor(private prisma: PrismaClient) {
+    this.delete = this.delete.bind(this);
+  }
+
+  async get(user_id: string) {
+    const recent = await this.prisma.recentCommunities.findMany({
+      where: { user_id },
+      orderBy: { interacted_at: 'desc' },
+      include: {
+        community: {
+          select: {
+            name: true,
+            profile_picture_url: true,
+          },
+        },
+      },
+    });
+
+    return recent;
+  }
+
+  async assign(user_id: string, community_id: string) {
+    const count = await this.prisma.recentCommunities.findMany({
+      where: { user_id },
+      orderBy: { interacted_at: 'desc' },
+      select: {
+        id: true,
+        interacted_at: true,
+        community: { select: { id: true, name: true } },
+      },
+    });
+
+    if (count.length >= 5) {
+      const firstRecent = count[0];
+      const lastRecent = count[4];
+
+      if (firstRecent.community.id === community_id) {
+        return;
+      }
+
+      await this.delete(user_id, lastRecent.community.id);
+    }
+
+    await this.prisma.recentCommunities.create({
+      data: { user_id, community_id },
+    });
+  }
+
+  async delete(user_id: string, community_id: string) {
+    await this.prisma.recentCommunities.delete({
+      where: {
+        community_id_user_id: {
+          community_id,
+          user_id,
+        },
+      },
+    });
+  }
+}
