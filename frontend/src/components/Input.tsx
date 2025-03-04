35d21e078f83557bdf71fae6efb4d01170737baf
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-03-04 14:42:42 +0100

    Add new parameters

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,23 +1,32 @@
 interface InputProps {
   value: string;
   setterFunc: React.Dispatch<React.SetStateAction<string>>;
+  placeholder: string;
+  maxLength?: number;
   className?: string;
-  styling?: boolean;
+  onFocus?: () => void;
+  onBlur?: () => void;
 }
 
 export default function Input({
   value,
   setterFunc,
+  placeholder,
+  maxLength,
   className,
-  styling = true,
+  onFocus,
+  onBlur,
 }: InputProps) {
   return (
-    // TODO: Add accessibility
     <input
-      className={`${className} ${styling ? 'h-[36px] rounded-full px-4' : ''}`}
-      type="text"
+      className={`${className} h-12 rounded-2xl px-4 focus-blue`}
       value={value}
+      placeholder={placeholder}
       onChange={(e) => setterFunc(e.target.value)}
+      type="text"
+      maxLength={maxLength}
+      onFocus={onFocus}
+      onBlur={onBlur}
     />
   );
 }
