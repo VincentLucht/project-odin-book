2d31df541d3a1496ffeb8ece8fd852701b237bdb
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-03-04 14:44:29 +0100

    Add object for sweetalert 2 default props

---
+++ frontend/src/util/swalDefaultProps.ts
@@ -0,0 +1,12 @@
+const swalDefaultProps = {
+  showCancelButton: true,
+  showClass: { popup: '' },
+  hideClass: { popup: '' },
+  background: '#333',
+  color: '#fff',
+  confirmButtonColor: '#3b82f6',
+  cancelButtonColor: '#ef4444',
+  reverseButtons: true,
+};
+
+export default swalDefaultProps;
