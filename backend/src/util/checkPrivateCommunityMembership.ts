920065c9cf525e0c397863c0e004c2f36f491583
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-03-04 14:43:48 +0100

    Fix not working due to returning the response, returns objects now that mimic the response

--- backend/src/util/checkPrivateCommunityMembership.ts
+++ backend/src/util/checkPrivateCommunityMembership.ts
@@ -1,44 +1,52 @@
 import { Post } from '@prisma/client/default';
 import { DB } from '@/db/db';
-import { Request, Response } from 'express';
+import { JwtPayload } from 'jsonwebtoken';
 
 import getAuthUser from '@/util/getAuthUser';
 
 export default async function checkPrivateCommunityMembership(
   db: DB,
   post: Post,
-  userId: undefined,
-  req: Request,
-  res: Response,
+  authData: string | JwtPayload | undefined,
+  returnUserId = false,
 ) {
   const community = await db.community.getById(post.community_id);
   if (!community) {
-    return res.status(404).json({ message: 'Community not found' });
+    return { ok: false, status: 404, message: 'Community not found' };
   }
 
   if (community.type === 'PRIVATE') {
-    if (!req.authData) {
-      return res.status(401).json({
+    if (!authData) {
+      return {
+        ok: false,
+        status: 401,
         message: 'Authentication required for private community content',
-      });
+      };
     }
 
-    const { user_id } = getAuthUser(req.authData);
-    userId = user_id;
+    const { user_id } = getAuthUser(authData);
 
     const isMember = await db.userCommunity.isMember(user_id, community.id);
     if (!isMember) {
-      return res
-        .status(403)
-        .json({ message: 'You are not part of this community' });
+      return {
+        ok: false,
+        status: 403,
+        message: 'You are not part of this community',
+      };
     }
 
     if (await db.bannedUsers.isBanned(user_id, community.id)) {
-      return res
-        .status(403)
-        .json({ message: 'You are banned from this community' });
+      return {
+        ok: false,
+        status: 403,
+        message: 'You are banned from this community',
+      };
+    }
+
+    if (returnUserId) {
+      return { ok: true, user_id };
     }
   }
 
-  return userId;
+  return { ok: true };
 }
