3fd0cd727de81d357f3b6b2ce2437b4442e1f849
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-03-07 12:33:17 +0100

    Add Add awareness if you were on a community page previously, and instantly select it

--- frontend/src/Header/components/CreateButton/CreateButton.tsx
+++ frontend/src/Header/components/CreateButton/CreateButton.tsx
@@ -1,10 +1,23 @@
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function CreateButton() {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleClick = () => {
+    // Check if on community page
+    const communityMatch = location.pathname.match(/\/r\/([^]+)/);
+
+    if (communityMatch) {
+      const communityName = communityMatch[1];
+      navigate(`/create?community=${communityName}`);
+    } else {
+      navigate('/create');
+    }
+  };
 
   return (
-    <div className="df bg-hover-transition" onClick={() => navigate('/create')}>
+    <div className="df bg-hover-transition" onClick={handleClick}>
       <img
         src="/plus.svg"
         alt="User profile picture"
