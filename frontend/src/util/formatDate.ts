ce906ef9e013cb46b380459f688629b5387300dc
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-03-07 11:20:31 +0100

    Add Date to type

--- frontend/src/util/formatDate.ts
+++ frontend/src/util/formatDate.ts
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs';
 
-export default function formatDate(date: string) {
+export default function formatDate(date: string | Date) {
   return dayjs(date).format('MMM D, YYYY');
 }
