d170caac591ecc310a98d81e6a64d393c7afb560
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-03-09 12:41:51 +0100

    Add component

---
+++ frontend/src/Main/Community/components/CommunityHeader/components/components/ChevronButton.tsx
@@ -0,0 +1,24 @@
+import { ChevronDownIcon } from 'lucide-react';
+
+interface ChevronButtonProps {
+  text: string;
+  customFunc: () => void;
+  className?: string;
+}
+
+export default function ChevronButton({
+  text,
+  customFunc,
+  className,
+}: ChevronButtonProps) {
+  return (
+    <button
+      className={`h-[32px] w-[59px] gap-1 rounded-full pl-3 pr-2 df bg-transition-hover ${className}`}
+      onClick={customFunc}
+    >
+      <span className="text-sm">{text}</span>
+
+      <ChevronDownIcon className="h-4 w-4 shrink-0" />
+    </button>
+  );
+}
