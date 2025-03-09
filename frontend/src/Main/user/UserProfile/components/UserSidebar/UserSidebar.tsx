318c9d1c0232aefc12cd3187c826b8d2547e6ce8
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-03-09 12:15:37 +0100

    Make sidebar sticky

--- frontend/src/Main/user/UserProfile/components/UserSidebar/UserSidebar.tsx
+++ frontend/src/Main/user/UserProfile/components/UserSidebar/UserSidebar.tsx
@@ -6,40 +6,37 @@ interface UserSidebarProps {
 
 export default function UserSideBar({ user }: UserSidebarProps) {
   if (!user) {
-    return;
+    return null;
   }
 
-  // TODO: Make the sidebar fixed and stay when scrolling
   return (
-    <div>
-      <div className="flex flex-col gap-2 rounded-2xl bg-black p-4">
-        <div className="flex justify-between">
-          <div className="font-semibold">{user.username}</div>
+    <div className="sticky top-4 flex h-fit flex-col gap-2 rounded-2xl bg-black p-4">
+      <div className="flex justify-between">
+        <div className="font-semibold">{user.username}</div>
 
-          <div>Dots</div>
+        <div>Dots</div>
+      </div>
+
+      <div className="flex gap-1">
+        <div>follow</div>
+
+        <div>chat</div>
+      </div>
+
+      <div className="flex justify-between">
+        <div className="flex flex-col">
+          <span className="text-sm">{user.post_karma}</span>
+          <span className="text-xs text-gray-secondary">Post karma</span>
         </div>
 
-        <div className="flex gap-1">
-          <div>follow</div>
-
-          <div>chat</div>
+        <div className="flex flex-col">
+          <span className="text-sm">{user.comment_karma}</span>
+          <span className="text-xs text-gray-secondary">Comment karma</span>
         </div>
 
-        <div className="flex justify-between">
-          <div className="flex flex-col">
-            <span className="text-sm">{user.post_karma}</span>
-            <span className="text-xs text-gray-secondary">Post karma</span>
-          </div>
-
-          <div className="flex flex-col">
-            <span className="text-sm">{user.comment_karma}</span>
-            <span className="text-xs text-gray-secondary">Comment karma</span>
-          </div>
-
-          <div className="flex flex-col">
-            <span className="text-sm">{user.cake_day}</span>
-            <span className="text-xs text-gray-secondary">Cake day</span>
-          </div>
+        <div className="flex flex-col">
+          <span className="text-sm">{user.cake_day}</span>
+          <span className="text-xs text-gray-secondary">Cake day</span>
         </div>
       </div>
     </div>
