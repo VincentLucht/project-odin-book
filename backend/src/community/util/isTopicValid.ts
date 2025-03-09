82e35d917792ab1929895ef677b83fbe51b1da2e
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-03-09 12:30:53 +0100

    Add not allowing more than 3 topics

--- backend/src/community/util/isTopicValid.ts
+++ backend/src/community/util/isTopicValid.ts
@@ -1,8 +1,12 @@
 import db from '@/db/db';
 
+// Format is ["topic ID"]
 export default async function isTopicValid(userTopics: string[]) {
   if (!userTopics || !Array.isArray(userTopics)) {
     throw new Error('Community Topics are required');
+  }
+  if (userTopics.length > 3) {
+    throw new Error('You can only choose 3 community topics');
   }
 
   const topics = await db.topic.getAll();
