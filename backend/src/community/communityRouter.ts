cd216cee74011807b8e0eb7deea879386cc7a7ed
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-03-09 12:29:21 +0100

    Add fetching communities with sorting

--- backend/src/community/communityRouter.ts
+++ backend/src/community/communityRouter.ts
@@ -3,11 +3,37 @@ import token from '@/auth/token';
 import communityValidator from '@/community/communityValidator';
 import communityController from '@/community/communityController';
 
-// /community is base path
 const communityRouter = express.Router();
 
+communityRouter.get(
+  '/community/is-name-available',
+  token.authenticate,
+  communityValidator.isNameAvailableRules(),
+  communityController.isNameAvailable,
+);
+
+communityRouter.get(
+  '/community/:community_name/search',
+  communityValidator.searchByNameRules(),
+  communityController.searchByName,
+);
+
+communityRouter.get(
+  '/community/:community_name/creation-info',
+  token.authenticateOptional,
+  communityValidator.getCreationInfoRules(),
+  communityController.getCreationInfo,
+);
+
+communityRouter.get(
+  '/r/:community_name/:sort_by_type?/:timeframe?',
+  token.authenticateOptional,
+  communityValidator.fetchRules(),
+  communityController.fetchWithPosts,
+);
+
 communityRouter.post(
-  '',
+  '/community',
   token.authenticate,
   communityValidator.creationRules(),
   communityController.create,
--- backend/src/community/communityValidator.ts
+++ backend/src/community/communityValidator.ts
@@ -1,19 +1,62 @@
 import { CommunityType } from '@prisma/client/default';
-import { body } from 'express-validator/lib';
+import { body, param, query } from 'express-validator/lib';
 import vm from '@/util/validationMessage';
 import isTopicValid from '@/community/util/isTopicValid';
+import isSortByValid from '@/util/isSortByValid';
 
 // prettier-ignore
 class CommunityValidator {
+  fetchRules() {
+    return [
+      param('community_name').trim()
+        .notEmpty()
+        .withMessage(vm.req('community name')),
+
+      param('sort_by_type').trim()
+        .custom((type: string) => {
+          return isSortByValid(type);
+        }),
+    ];
+  }
+
+  searchByNameRules() {
+    return [
+      param('community_name').trim()
+        .notEmpty()
+        .withMessage(vm.req('name')),
+  ];
+  }
+
+  getCreationInfoRules() {
+    return [
+      param('community_name').trim()
+        .notEmpty()
+        .withMessage(vm.req('name')),
+      query('get_membership')
+        .optional()
+        .isBoolean(),
+    ];
+  }
+
+  isNameAvailableRules() {
+    return [
+      query('community_name').trim()
+        .notEmpty()
+        .withMessage(vm.req('Community Name')),
+    ];
+  }
+
   creationRules() {
     return [
       body('name').trim()
-        .isLength({ min: 1 })
-        .withMessage(vm.minLen('Name', 1))
-        .isLength({ max: 30 })
-        .withMessage(vm.maxLen('Name', 30)),
+        .isLength({ min: 3 })
+        .withMessage(vm.minLen('Name', 3))
+        .isLength({ max: 21 })
+        .withMessage(vm.maxLen('Name', 21)),
 
       body('description').trim()
+        .notEmpty()
+        .withMessage(vm.req('Description'))
         .isLength({ max: 300 })
         .withMessage(vm.maxLen('Description', 300)),
 
