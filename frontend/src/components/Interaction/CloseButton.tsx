5f170d990312a87bfcf4d6ce2f025c3b966bbd43
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-03-14 15:45:34 +0100

    Add secondary mode, without outline

--- frontend/src/components/Interaction/CloseButton.tsx
+++ frontend/src/components/Interaction/CloseButton.tsx
@@ -1,14 +1,32 @@
 interface CloseButtonProps {
   className?: string;
   classNameSvg?: string;
+  outline?: boolean;
   customFunc: () => void;
 }
 
 export default function CloseButton({
   className,
   classNameSvg,
+  outline = true,
   customFunc,
 }: CloseButtonProps) {
+  if (!outline) {
+    return (
+      <button
+        className={`h-8 w-8 rounded-full p-1 transition-transform df hover:scale-[107%] active:scale-95
+          ${className}`}
+        onClick={customFunc}
+      >
+        <img
+          src="/x-close-red.svg"
+          alt="Close button"
+          className={`h-10 w-10 ${classNameSvg}`}
+        />
+      </button>
+    );
+  }
+
   return (
     <button
       className={`h-8 w-8 rounded-full p-1 transition-transform df normal-bg-transition active:scale-95
