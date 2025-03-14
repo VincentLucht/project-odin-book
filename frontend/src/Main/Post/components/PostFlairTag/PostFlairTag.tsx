a8ebc2ebb826d4a7c0f200318334cc8a1f377767
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-03-14 15:44:47 +0100

    Make text unselectable

--- frontend/src/Main/Post/components/PostFlairTag/PostFlairTag.tsx
+++ frontend/src/Main/Post/components/PostFlairTag/PostFlairTag.tsx
@@ -19,7 +19,7 @@ export default function PostFlairTag({
 
   return (
     <div
-      className={`w-fit gap-1 rounded-full px-2 text-xs df ${className}`}
+      className={`w-fit cursor-default select-none gap-1 rounded-full px-2 text-xs df ${className}`}
       style={{ backgroundColor: flair.color, color: flair.textColor }}
     >
       <span>{flair.name}</span>
