5fc9a6ab7b8514e834de1eaff318b95db9681cf5
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-03-19 10:54:04 +0100

    Create new date object to avoid mutability

--- backend/src/db/managers/util/getStartDate.ts
+++ backend/src/db/managers/util/getStartDate.ts
@@ -4,26 +4,25 @@ export default function getStartDate(timeframe: TimeFrame) {
   const now = new Date();
 
   // Calculate the start date based on the timeframe
-  let startDate: Date | null = null;
+  const startDate = new Date(now);
+
   switch (timeframe) {
     case 'day':
-      startDate = new Date(now.setDate(now.getDate() - 1));
+      startDate.setDate(startDate.getDate() - 1);
       break;
     case 'week':
-      startDate = new Date(now.setDate(now.getDate() - 7));
+      startDate.setDate(startDate.getDate() - 7);
       break;
     case 'month':
-      startDate = new Date(now.setMonth(now.getMonth() - 1));
+      startDate.setMonth(startDate.getMonth() - 1);
       break;
     case 'year':
-      startDate = new Date(now.setFullYear(now.getFullYear() - 1));
+      startDate.setFullYear(startDate.getFullYear() - 1);
       break;
     case 'all':
-      startDate = null;
-      break;
+      return null;
     default:
-      startDate = null;
-      break;
+      return null;
   }
 
   return startDate;
