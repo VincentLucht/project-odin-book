30ce2450911aafcc0dd87d73bb1b742b0bdcf7d1
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-03-24 11:16:56 +0100

    Update import

--- frontend/src/components/sidebar/DisplayMemberCount.tsx/DisplayMemberCount.tsx
+++ frontend/src/components/sidebar/DisplayMemberCount.tsx/DisplayMemberCount.tsx
@@ -1,4 +1,4 @@
-import getMemberCount from '@/components/sidebar/DisplayMemberCount.tsx/getMembercount';
+import formatCount from '@/components/sidebar/DisplayMemberCount.tsx/formatCount';
 
 interface DisplayMemberCountProps {
   memberCount: number;
@@ -7,7 +7,7 @@ interface DisplayMemberCountProps {
 export default function DisplayMemberCount({ memberCount }: DisplayMemberCountProps) {
   return (
     <div className="flex flex-col">
-      <span className="font-semibold">{getMemberCount(memberCount)}</span>
+      <span className="font-semibold">{formatCount(memberCount)}</span>
 
       <span className="-mt-[2px] text-sm font-light">Members</span>
     </div>
