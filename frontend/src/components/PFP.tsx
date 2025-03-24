5b82f0e48fa755433d0add890b847b536b9425dc
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-03-24 11:13:29 +0100

    Update component name, add more options

---
+++ frontend/src/components/PFP.tsx
@@ -0,0 +1,30 @@
+interface CommunityPFPProps {
+  src: string | undefined | null;
+  size?: 'normal' | 'large' | 'xl';
+  mode?: 'community' | 'user';
+  className?: string;
+}
+
+const sizeClasses = {
+  normal: 'h-6 w-6',
+  large: 'h-9 w-9',
+  xl: 'h-12 w-12',
+};
+
+export default function PFP({
+  src,
+  size = 'normal',
+  mode = 'community',
+  className,
+}: CommunityPFPProps) {
+  const pfp =
+    mode === 'user' ? (src ?? '/user-default.svg') : (src ?? '/community-default.svg');
+
+  return (
+    <img
+      src={pfp}
+      alt={`${mode === 'community' ? 'Community Profile Picture' : 'User Profile Picture'}`}
+      className={`${sizeClasses[size] ?? sizeClasses.normal} ${className} rounded-full border`}
+    />
+  );
+}
