2c31c949a5d397f462b30362012dfc1af2f8efcf
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-03-24 11:16:36 +0100

    Add className

--- frontend/src/components/Separator.tsx
+++ frontend/src/components/Separator.tsx
@@ -1,11 +1,12 @@
 interface SeparatorProps {
+  className?: string;
   mode?: 'normal' | 'sidebar';
 }
 
-export default function Separator({ mode = 'normal' }: SeparatorProps) {
+export default function Separator({ className, mode = 'normal' }: SeparatorProps) {
   return (
     <hr
-      className={`h-[1px] border-0 bg-gray-600 ${mode === 'sidebar' && 'my-3 w-[215px]'}`}
+      className={`h-[1px] border-0 bg-gray-600 ${mode === 'sidebar' && 'my-3 w-[215px]'} ${className}`}
     />
   );
 }
