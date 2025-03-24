7e27350e5a72b3a991e703619f58382113420cd1
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-03-24 11:18:34 +0100

    Add safe search

--- frontend/src/Main/Community/components/CommunityHeader/components/SetSortByType.tsx
+++ frontend/src/Main/Community/components/CommunityHeader/components/SetSortByType.tsx
@@ -11,10 +11,14 @@ import { SortByType } from '@/Main/Community/Community';
 import { TimeFrame } from '@/Main/Community/Community';
 
 interface SetSortByTypeProps {
-  sortByType: SortByType;
-  setSortByType: React.Dispatch<React.SetStateAction<SortByType>>;
+  sortByType: SortByType | 'relevance';
+  setSortByType: (sortBy: SortByType) => void;
   timeframe: TimeFrame;
   setTimeframe: React.Dispatch<React.SetStateAction<TimeFrame>>;
+  excludeSortOptions: boolean;
+  mode?: 'community' | 'search';
+  safeSearch?: boolean;
+  setSafeSearch?: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export default function SetSortByType({
@@ -22,20 +26,31 @@ export default function SetSortByType({
   setSortByType,
   timeframe,
   setTimeframe,
+  excludeSortOptions,
+  mode = 'community',
+  safeSearch,
+  setSafeSearch,
 }: SetSortByTypeProps) {
-  const [showDropdown, setShowDropdown] = useState<'sortBy' | 'timeframe' | null>(null);
+  const [showDropdown, setShowDropdown] = useState<
+    'sortBy' | 'timeframe' | 'safeSearch' | null
+  >(null);
 
   const containerRef = useClickOutside(() => {
     setShowDropdown(null);
   });
 
-  const onSortByClick = (sortBy: SortByType) => {
-    setSortByType(sortBy);
+  const onSortByClick = (sortBy: SortByType | 'relevance') => {
+    setSortByType(sortBy as SortByType);
     setShowDropdown(null);
   };
 
   const onTimeframeClick = (newTimeframe: TimeFrame) => {
     setTimeframe(newTimeframe);
+    setShowDropdown(null);
+  };
+
+  const onSafeSearchClick = (safeSearch: boolean) => {
+    setSafeSearch && setSafeSearch(safeSearch);
     setShowDropdown(null);
   };
 
@@ -45,45 +60,56 @@ export default function SetSortByType({
   const showTimeframe = showDropdown === 'timeframe';
 
   return (
-    <div ref={containerRef} className={`mb-2 w-fit ${sortByType === 'top' && 'flex'}`}>
-      <div>
-        <ChevronButton
-          customFunc={() =>
-            setShowDropdown((prev) => (prev === 'sortBy' ? null : 'sortBy'))
-          }
-          text={sortByDisplay}
-          className={`${showDropdown === 'sortBy' && 'bg-accent-gray'}`}
-        />
-        <DropdownMenu
-          className={`-ml-[10px] -mt-5 w-20 rounded-md transition-opacity duration-300 df
-            ${showDropdown === 'sortBy' ? 'z-10 opacity-100' : '-z-10 opacity-0'} `}
-        >
-          <span className="mb-1 cursor-default select-none">Sort by</span>
-
-          <DropdownButton
-            text="Hot"
-            show={showSortBy}
-            isSelected={sortByType === 'hot'}
-            customFunc={() => onSortByClick('hot')}
+    <div ref={containerRef} className={'mb-2 flex w-fit'}>
+      {!excludeSortOptions && (
+        <div>
+          <ChevronButton
+            customFunc={() =>
+              setShowDropdown((prev) => (prev === 'sortBy' ? null : 'sortBy'))
+            }
+            text={sortByDisplay}
+            className={`${showDropdown === 'sortBy' && 'bg-accent-gray'}`}
           />
+          <DropdownMenu
+            className={`-mt-5 ${mode === 'community' ? 'w-20' : 'w-[106px]'} rounded-md transition-opacity
+            duration-300 df ${showDropdown === 'sortBy' ? 'z-10 opacity-100' : '-z-10 opacity-0'} `}
+          >
+            <span className="mb-1 cursor-default select-none">Sort by</span>
 
-          <DropdownButton
-            text="New"
-            show={showSortBy}
-            isSelected={sortByType === 'new'}
-            customFunc={() => onSortByClick('new')}
-          />
+            {mode === 'community' ? (
+              <DropdownButton
+                text="Hot"
+                show={showSortBy}
+                isSelected={sortByType === 'hot'}
+                customFunc={() => onSortByClick('hot')}
+              />
+            ) : (
+              <DropdownButton
+                text="Relevance"
+                show={showSortBy}
+                isSelected={sortByType === 'relevance'}
+                customFunc={() => onSortByClick('relevance')}
+              />
+            )}
 
-          <DropdownButton
-            text="Top"
-            show={showSortBy}
-            isSelected={sortByType === 'top'}
-            customFunc={() => onSortByClick('top')}
-          />
-        </DropdownMenu>
-      </div>
+            <DropdownButton
+              text="New"
+              show={showSortBy}
+              isSelected={sortByType === 'new'}
+              customFunc={() => onSortByClick('new')}
+            />
 
-      {sortByType === 'top' && (
+            <DropdownButton
+              text="Top"
+              show={showSortBy}
+              isSelected={sortByType === 'top'}
+              customFunc={() => onSortByClick('top')}
+            />
+          </DropdownMenu>
+        </div>
+      )}
+
+      {!excludeSortOptions && (sortByType === 'top' || sortByType === 'relevance') && (
         <div>
           <ChevronButton
             customFunc={() =>
@@ -93,7 +119,7 @@ export default function SetSortByType({
             className={`!w-fit ${showDropdown === 'timeframe' && 'bg-accent-gray'}`}
           />
           <DropdownMenu
-            className={`-ml-[10px] -mt-5 w-[112px] rounded-md transition-opacity duration-300 df
+            className={`-mt-5 w-[112px] rounded-md transition-opacity duration-300 df
             ${showDropdown === 'timeframe' ? 'z-10 opacity-100' : '-z-10 opacity-0'} `}
           >
             <span className="mb-1 cursor-default select-none">Sort by</span>
@@ -131,6 +157,36 @@ export default function SetSortByType({
               show={showTimeframe}
               isSelected={timeframe === 'all'}
               customFunc={() => onTimeframeClick('all')}
+            />
+          </DropdownMenu>
+        </div>
+      )}
+
+      {mode === 'search' && (
+        <div>
+          <ChevronButton
+            customFunc={() =>
+              setShowDropdown((prev) => (prev === 'safeSearch' ? null : 'safeSearch'))
+            }
+            text={`Safe Search ${safeSearch ? 'On' : 'Off'}`}
+            className={`!w-fit ${showDropdown === 'safeSearch' && 'bg-accent-gray'}`}
+          />
+          <DropdownMenu
+            className={`-mt-5 w-[112px] rounded-md transition-opacity duration-300 df
+            ${showDropdown === 'safeSearch' ? 'z-10 opacity-100' : '-z-10 opacity-0'} `}
+          >
+            <DropdownButton
+              text="On"
+              show={mode === 'search'}
+              isSelected={safeSearch}
+              customFunc={() => onSafeSearchClick(true)}
+            />
+
+            <DropdownButton
+              text="Off"
+              show={mode === 'search'}
+              isSelected={!safeSearch}
+              customFunc={() => onSafeSearchClick(false)}
             />
           </DropdownMenu>
         </div>
