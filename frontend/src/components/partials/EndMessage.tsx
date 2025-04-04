633dd4a94bc7e271316e56913a80b903750b66e0
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-04-04 11:55:56 +0200

    Add classname

--- frontend/src/components/partials/EndMessage.tsx
+++ frontend/src/components/partials/EndMessage.tsx
@@ -1,6 +1,10 @@
-export default function EndMessage() {
+interface EndMessageProps {
+  className?: string;
+}
+
+export default function EndMessage({ className }: EndMessageProps) {
   return (
-    <div className="pb-5 pt-3 text-center text-lg font-semibold">
+    <div className={`${className} pb-5 pt-3 text-center text-lg font-semibold`}>
       You&apos;ve reached the end!
     </div>
   );
