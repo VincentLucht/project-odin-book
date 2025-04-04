db04d7d8442ea248e5ef179187a96cc0793778f5
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-04-04 11:53:41 +0200

    Add type for pagination

--- frontend/src/interface/backendTypes.ts
+++ frontend/src/interface/backendTypes.ts
@@ -3,3 +3,8 @@ export type SortBy = 'new' | 'top' | 'hot';
 export type SortByUser = 'new' | 'top';
 
 export type VoteType = 'UPVOTE' | 'DOWNVOTE';
+
+export interface Pagination {
+  nextCursor: string;
+  hasMore: boolean;
+}
