90ca95fae67243578d4578daef7e6703f69a19d9
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-04-14 12:58:50 +0200

    Add global API function

---
+++ frontend/src/util/apiRequest.ts
@@ -0,0 +1,34 @@
+import API_URL from '@/auth/ApiUrl';
+
+export default async function apiRequest<T>(
+  endpoint: string,
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+  token: string | null,
+  body?: any,
+): Promise<T> {
+  const response = await fetch(`${API_URL}${endpoint}`, {
+    method,
+    headers: {
+      'content-type': 'application/json',
+      ...(token && { authorization: token }),
+    },
+    ...(body && { body: JSON.stringify(body) }),
+  } as RequestInit);
+
+  if (!response.ok) {
+    const errorObject = (await response.json()) as string;
+    throw errorObject;
+  }
+
+  // Check if there's content to parse
+  const contentType = response.headers.get('content-type');
+  if (response.status === 204 || !contentType?.includes('application/json')) {
+    return {} as T;
+  }
+
+  try {
+    return (await response.json()) as T;
+  } catch (e) {
+    return {} as T;
+  }
+}
