029b2b5e8f9da8574c2ba3bbf6d191bade5a46b0
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-04-15 11:49:10 +0200

    Add component

---
+++ frontend/src/Main/Global/MemberShipButton.tsx
@@ -0,0 +1,33 @@
+interface MemberShipButtonProps {
+  isMember: boolean;
+  onClick: () => void;
+  classNameMember?: string;
+  classNameNotMember?: string;
+}
+
+export default function MemberShipButton({
+  isMember,
+  onClick,
+  classNameMember,
+  classNameNotMember,
+}: MemberShipButtonProps) {
+  return (
+    <div className="transition-all">
+      {isMember ? (
+        <button
+          className={`h-[38px] max-w-[65px] !px-10 !font-medium transparent-btn ${classNameMember}`}
+          onClick={onClick}
+        >
+          Joined
+        </button>
+      ) : (
+        <button
+          className={`h-[38px] max-w-[50px] !px-7 !font-medium df prm-button-blue ${classNameNotMember}`}
+          onClick={onClick}
+        >
+          Join
+        </button>
+      )}
+    </div>
+  );
+}
