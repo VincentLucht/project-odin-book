2c0b200732eb5034cdb2a5a02917d99600b0203d
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-04-20 11:00:37 +0200

    Add being able to disable hover effect

--- frontend/src/components/user/UserCard.tsx
+++ frontend/src/components/user/UserCard.tsx
@@ -1,16 +1,17 @@
-import CommunityFlairTag from '@/components/CommunityFlairTag';
+import CommunityFlairTag from '@/Main/Global/CommunityFlairTag';
 import { NavigateFunction } from 'react-router-dom';
 
 interface UserCardProps {
   profile_picture_url: string | null;
   username: string;
-  navigate: NavigateFunction;
+  navigate?: NavigateFunction;
   userFlair?: {
     name: string;
     emoji: string | null;
     textColor: string;
     color: string;
   };
+  hoverEffect?: boolean;
 }
 
 export default function UserCard({
@@ -18,16 +19,17 @@ export default function UserCard({
   username,
   navigate,
   userFlair,
+  hoverEffect = false,
 }: UserCardProps) {
   return (
     <div
-      className="-mx-2 flex h-12 cursor-pointer items-center gap-2 rounded-md px-2 text-sm
-        transition-colors bg-transition-hover-2"
-      onClick={() => navigate(`/user/${username}`)}
+      className={`-mx-2 flex h-12 items-center gap-2 rounded-md px-2 text-sm ${ hoverEffect &&
+        'cursor-pointer transition-colors bg-transition-hover-2' }`}
+      onClick={() => navigate?.(`/user/${username}`)}
     >
       <img
         src={profile_picture_url ? profile_picture_url : 'user.svg'}
-        alt="Community moderator profile picture"
+        alt="User profile picture"
         className="h-8 w-8 rounded-full border object-cover"
       />
 
