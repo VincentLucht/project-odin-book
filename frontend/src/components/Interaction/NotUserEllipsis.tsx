df3ccb208c643664babb42e9fb81dbca4376eae3
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-04-25 15:49:07 +0200

    Add component for when user is not poster - allows for reports and saving posts

---
+++ frontend/src/components/Interaction/NotUserEllipsis.tsx
@@ -0,0 +1,104 @@
+import { useState, useRef } from 'react';
+import useClickOutside from '@/hooks/useClickOutside';
+
+import DropdownMenu from '@/components/DropdownMenu/DropdownMenu';
+import DropdownButton from '@/components/DropdownMenu/components/DropdownButton';
+import ReportModal from '@/Main/Global/ReportModal';
+import { EllipsisIcon, BookmarkIcon, FlagIcon } from 'lucide-react';
+
+import { UserAndHistory } from '@/Main/user/UserProfile/api/fetchUserProfile';
+import { DBPostWithCommunityName, DBPostWithCommunity } from '@/interface/dbSchema';
+
+interface NotUserEllipsisProps {
+  hasSaved: boolean;
+  hasReported: boolean;
+  token: string | null;
+  id: string;
+  mode?: 'post' | 'comment';
+  showDropdown: string | null;
+  setShowDropdown: React.Dispatch<React.SetStateAction<string | null>>;
+  setFetchedUser?: React.Dispatch<React.SetStateAction<UserAndHistory | null>>;
+  setPosts?: React.Dispatch<React.SetStateAction<DBPostWithCommunityName[]>>;
+  setPost: React.Dispatch<React.SetStateAction<DBPostWithCommunity | null>>;
+}
+
+export default function NotUserEllipsis({
+  hasSaved = false, // TODO: Remove this
+  hasReported,
+  token,
+  id,
+  mode = 'post',
+  showDropdown,
+  setShowDropdown,
+  setFetchedUser,
+  setPosts,
+  setPost,
+}: NotUserEllipsisProps) {
+  const [showReportModal, setShowReportModal] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+  const divRef = useClickOutside(() => {
+    setShowDropdown(null);
+  }, dropdownRef);
+
+  const show = showDropdown === id;
+
+  return (
+    <div className="relative" ref={divRef}>
+      <div
+        className={`cursor-pointer px-3 transition-all interaction-button-wrapper-secondary
+          hover:bg-hover-gray active:bg-active-gray ${ mode === 'comment' &&
+          'text-gray-400 hover:text-white' }`}
+        onClick={() => setShowDropdown((prev) => (prev === id ? null : id))}
+        onMouseDown={(e) => e.stopPropagation()}
+      >
+        <EllipsisIcon className="h-[18px] w-[18px]" />
+      </div>
+
+      <DropdownMenu
+        className={`!-left-[216px] !top-9 min-w-[256px] rounded-md text-white transition-opacity
+          duration-300 df ${show ? '!z-10 opacity-100' : '!-z-10 opacity-0'} `}
+        ref={dropdownRef}
+      >
+        <DropdownButton
+          text={
+            hasSaved
+              ? `Saved ${mode}`
+              : `${mode === 'post' ? 'Save post' : 'Save comment'}`
+          }
+          icon={<BookmarkIcon />}
+          alt={
+            hasSaved
+              ? `Saved ${mode}`
+              : `${mode === 'post' ? 'Save post' : 'Save comment'}`
+          }
+          imgClassName="rounded-full border h-[32px] w-[32px]"
+          setterFunc={setShowDropdown}
+          show={show}
+        />
+
+        <DropdownButton
+          text={hasReported ? `Already reported ${mode}` : `Report ${mode}`}
+          icon={<FlagIcon />}
+          alt={hasReported ? `Already reported ${mode}` : `Report ${mode}`}
+          imgClassName="rounded-full border h-8 w-8"
+          setterFunc={setShowDropdown}
+          customFunc={() => {
+            if (hasReported) return;
+            setShowReportModal(true);
+          }}
+          show={show}
+        />
+      </DropdownMenu>
+
+      <ReportModal
+        show={showReportModal}
+        onClose={() => setShowReportModal(false)}
+        token={token}
+        apiData={{ type: mode.toUpperCase() as 'POST' | 'COMMENT', item_id: id }}
+        setFetchedUser={setFetchedUser}
+        setPosts={setPosts}
+        setPost={setPost}
+      />
+    </div>
+  );
+}
