556c4f044a6b5b8dc3a45256ed8bbd1c9556d280
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-06-09 12:50:55 +0200

    Add hook for generating random widths for skeleton loading components

---
+++ frontend/src/Main/Post/components/Loading/hook/useSkeletonRandomValuesProps.ts
@@ -0,0 +1,27 @@
+import { useMemo } from 'react';
+import getBiasedRandomNumber from '@/util/getBiasedRandomNumber';
+
+interface UseSkeletonRandomValuesProps {
+  communityNameLengthProp?: number;
+}
+
+export function useSkeletonRandomValues({
+  communityNameLengthProp,
+}: UseSkeletonRandomValuesProps = {}) {
+  const randomValues = useMemo(
+    () => ({
+      communityNameLength:
+        communityNameLengthProp ?? Math.floor(Math.random() * (100 - 30 + 1)) + 30,
+      commentLength: Math.floor(Math.random() * 10) + 1,
+      usernameLength: Math.floor(Math.random() * (170 - 20 + 1)) + 20,
+      postTitleLength: getBiasedRandomNumber(),
+      contentLines: Math.floor(Math.random() * 10) + 3, // 3-12 randint
+      isMature: Math.random() < 0.05,
+      isSpoiler: Math.random() < 0.05,
+      lastLineRandom: Math.floor(Math.random() * (80 - 20 + 1)) + 20,
+    }),
+    [communityNameLengthProp],
+  );
+
+  return randomValues;
+}
