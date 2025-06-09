fccf238220469ddf0715d41b5dccf7bca8451f5f
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-06-09 12:52:36 +0200

    Replace with hook, match post better

--- frontend/src/Main/Post/components/Loading/PostLazy.tsx
+++ frontend/src/Main/Post/components/Loading/PostLazy.tsx
@@ -1,35 +1,20 @@
-import { useMemo } from 'react';
+import { useSkeletonRandomValues } from '@/Main/Post/components/Loading/hook/useSkeletonRandomValuesProps';
 
 import PFPLazy from '@/components/Lazy/PFPLazy';
 import LazyCompartment from '@/components/Lazy/LazyCompartment';
 import LazyTags from '@/components/Lazy/LazyTags';
 import LazyPostFlair from '@/components/Lazy/LazyPostFlair';
-
-import getBiasedRandomNumber from '@/util/getBiasedRandomNumber';
+import Separator from '@/components/Separator';
 
 interface PostLazyProps {
-  mode?: 'fetched' | 'normal';
-  communityNameLengthProp: number;
+  mode?: 'fetched' | 'normal' | 'overview';
+  communityNameLengthProp?: number;
 }
 
 export default function PostLazy({
   mode = 'normal',
   communityNameLengthProp,
 }: PostLazyProps) {
-  const randomValues = useMemo(
-    () => ({
-      communityNameLength: (communityNameLengthProp + 2) * 6,
-      commentLength: Math.floor(Math.random() * 10) + 1,
-      usernameLength: Math.floor(Math.random() * (170 - 20 + 1)) + 20,
-      postTitleLength: getBiasedRandomNumber(),
-      contentLines: Math.floor(Math.random() * 10) + 3, // 3-12 randint
-      isMature: Math.random() < 0.05,
-      isSpoiler: Math.random() < 0.05,
-      lastLineRandom: Math.floor(Math.random() * (80 - 20 + 1)) + 20,
-    }),
-    [communityNameLengthProp],
-  );
-
   const {
     communityNameLength,
     usernameLength,
@@ -38,70 +23,84 @@ export default function PostLazy({
     isMature,
     isSpoiler,
     lastLineRandom,
-  } = randomValues;
+  } = useSkeletonRandomValues({ communityNameLengthProp });
+
+  const isUserProfileMode = mode === 'overview';
 
   return (
-    <div className="p-4 center-main">
-      <div className={`center-main-content ${mode === 'fetched' && '!block'} `}>
-        <div className="flex flex-col">
-          <div className="flex gap-1 text-sm">
-            <PFPLazy className="!h-9 !w-9" />
+    <>
+      {isUserProfileMode && <Separator />}
 
-            <div className="flex-1 text-xs">
-              <div className="flex items-center gap-1">
-                {/* Community Name */}
-                <LazyCompartment height={20} width={communityNameLength} />
+      <div className={`${!isUserProfileMode ? 'p-4 center-main' : 'my-[6px] p-2'}`}>
+        <div
+          className={` ${mode === 'fetched' && '!block'} ${!isUserProfileMode && 'center-main-content'}`}
+        >
+          <div className="flex flex-col">
+            <div className="flex gap-1 text-sm">
+              <PFPLazy
+                className={`${!isUserProfileMode ? '!h-9 !w-9' : '!h-6 !w-6'}`}
+              />
 
-                {/* Creation Date */}
-                <LazyCompartment height={14} width={84} />
+              <div
+                className={`flex-1 text-xs ${isUserProfileMode && 'item-center flex'}`}
+              >
+                <div className="flex items-center gap-1">
+                  {/* Community Name */}
+                  <LazyCompartment height={20} width={communityNameLength} />
+
+                  {/* Creation Date */}
+                  <LazyCompartment height={14} width={84} />
+                </div>
+
+                {/* Username */}
+                {!isUserProfileMode && (
+                  <LazyCompartment
+                    height={12}
+                    width={usernameLength}
+                    className="mt-[4px]"
+                  />
+                )}
               </div>
 
-              {/* Username */}
-              <LazyCompartment
-                height={12}
-                width={usernameLength}
-                className="mt-[4px]"
-              />
+              <LazyCompartment className="mr-4 !rounded-full" height={22} width={77} />
+
+              {/* Ellipsis */}
+              <LazyCompartment height={22} width={22} />
             </div>
 
-            {/* Ellipsis */}
-            <LazyCompartment height={22} width={22} />
+            {/* Tags */}
+            <LazyTags isMature={isMature} isSpoiler={isSpoiler} className="mb-0 mt-2" />
+
+            {/* Title */}
+            <LazyCompartment height={28 * postTitleLength} className="mt-2" />
+
+            {/* Post Flair */}
+            <LazyPostFlair className="mt-2" />
+
+            {/* Content */}
+            <div className="!mb-4 !mt-2">
+              {Array.from({ length: contentLines }).map((_, i) => (
+                <div
+                  key={i}
+                  className={`skeleton h-6 ${i === 0 && 'rounded-t-md'} ${i === contentLines - 1 && 'rounded-b-md'}
+                  ${i === contentLines - 2 && 'rounded-bl-none rounded-br-md'} `}
+                  style={i === contentLines - 1 ? { width: `${lastLineRandom}%` } : {}}
+                />
+              ))}
+            </div>
+
+            {/* Interaction bar */}
+            <div className="flex items-center gap-3">
+              <LazyCompartment height={28} width={84} className="!rounded-full" />
+              <LazyCompartment height={28} width={76} className="!rounded-full" />
+              <LazyCompartment height={28} width={85} className="!rounded-full" />
+            </div>
           </div>
 
-          {/* Tags */}
-          <LazyTags isMature={isMature} isSpoiler={isSpoiler} className="mb-0 mt-2" />
-
-          {/* Title */}
-          <LazyCompartment height={28 * postTitleLength} className="mt-2" />
-
-          {/* Post Flair */}
-          <LazyPostFlair className="mt-2" />
-
-          {/* Content */}
-          <div className="!mb-4 !mt-2">
-            {Array.from({ length: contentLines }).map((_, i) => (
-              <div
-                key={i}
-                className={`skeleton h-6 ${i === 0 && 'rounded-t-md'} ${i === contentLines - 1 && 'rounded-b-md'}
-                ${i === contentLines - 2 && 'rounded-bl-none rounded-br-md'} `}
-                style={i === contentLines - 1 ? { width: `${lastLineRandom}%` } : {}}
-              />
-            ))}
-          </div>
-
-          {/* Interaction bar */}
-          <div className="flex items-center gap-3">
-            <LazyCompartment height={28} width={84} className="!rounded-full" />
-            <LazyCompartment height={28} width={76} className="!rounded-full" />
-            <LazyCompartment height={28} width={85} className="!rounded-full" />
-          </div>
-
-          {/* Comment section => separate? */}
+          {/* TODO: Complete this when done with sidebar */}
+          {/* Sidebar */}
         </div>
-
-        {/* TODO: Complete this when done with sidebar */}
-        {/* Sidebar */}
       </div>
-    </div>
+    </>
   );
 }
