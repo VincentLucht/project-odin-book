4dff0092e57f5377a753b458bdd44c4eb31ef46f
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-06-11 16:43:23 +0200

    Use min-h instead of fixed height

--- frontend/src/Main/Community/components/CommunityHeader/components/components/ChevronButton.tsx
+++ frontend/src/Main/Community/components/CommunityHeader/components/components/ChevronButton.tsx
@@ -13,7 +13,7 @@ export default function ChevronButton({
 }: ChevronButtonProps) {
   return (
     <button
-      className={`h-[32px] w-fit gap-1 rounded-full pl-3 pr-2 df bg-transition-hover ${className}`}
+      className={`min-h-[32px] w-fit gap-1 rounded-full pl-3 pr-2 df bg-transition-hover ${className}`}
       onClick={customFunc}
     >
       <span className="text-sm">{text}</span>
