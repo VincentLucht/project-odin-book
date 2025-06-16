39d9fa291a16a92ee748114bf7eea2171043a695
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-06-16 14:05:05 +0200

    Reduce padding on small screens

--- frontend/src/components/Interaction/ModMenuButton.tsx
+++ frontend/src/components/Interaction/ModMenuButton.tsx
@@ -7,7 +7,7 @@ interface ModMenuButtonProps {
 export default function ModMenuButton({ onClick }: ModMenuButtonProps) {
   return (
     <button
-      className="h-8 rounded-full px-[10px] hover:bg-hover-gray active:bg-active-gray"
+      className="h-8 rounded-full px-1 hover:bg-hover-gray active:bg-active-gray sm:px-[10px]"
       onClick={onClick}
       onMouseDown={(e) => e.stopPropagation()}
     >
