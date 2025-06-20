a25bf103ade2eae66b679e4e0e63f75b80994551
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-06-20 12:51:07 +0200

    Use PFP component instead image, use button html

--- frontend/src/components/user/UserCard.tsx
+++ frontend/src/components/user/UserCard.tsx
@@ -1,4 +1,6 @@
 import CommunityFlairTag from '@/Main/Global/CommunityFlairTag';
+import PFP from '@/components/PFP';
+
 import { NavigateFunction } from 'react-router-dom';
 
 interface UserCardProps {
@@ -22,22 +24,20 @@ export default function UserCard({
   hoverEffect = false,
 }: UserCardProps) {
   return (
-    <div
-      className={`-mx-2 flex h-12 items-center gap-2 rounded-md px-2 text-sm ${ hoverEffect &&
+    <button
+      className={`group -mx-2 flex h-12 items-center gap-2 rounded-md px-2 text-sm ${ hoverEffect &&
         'cursor-pointer transition-colors bg-transition-hover-2' }`}
       onClick={() => navigate?.(`/user/${username}`)}
     >
-      <img
-        src={profile_picture_url ? profile_picture_url : 'user.svg'}
-        alt="User profile picture"
-        className="h-8 w-8 rounded-full border object-cover"
-      />
+      <div className="bg-hover-transition">
+        <PFP src={profile_picture_url} mode="user" className="h-8 w-8" />
+      </div>
 
       <div>
-        <div>u/{username}</div>
+        <div className="group-hover:underline">u/{username}</div>
 
         {userFlair && <CommunityFlairTag flair={userFlair} />}
       </div>
-    </div>
+    </button>
   );
 }
