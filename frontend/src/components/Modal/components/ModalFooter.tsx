16adbbe1aedeba2ce7b04b6ebc3bd7728158a83d
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-06-20 13:53:23 +0200

    Add more options

--- frontend/src/components/Modal/components/ModalFooter.tsx
+++ frontend/src/components/Modal/components/ModalFooter.tsx
@@ -4,32 +4,49 @@ interface ModalFooterProps {
   onClose: () => void;
   submitting: boolean;
   onClick?: () => void;
+  cancelButtonName?: string;
   confirmButtonName?: string;
   confirmButtonClassName?: string;
   cancelButtonClassName?: string;
+  options?: {
+    cancelButton: boolean;
+    confirmButton: boolean;
+  };
 }
 
 export default function ModalFooter({
   onClose,
   submitting,
   onClick,
+  cancelButtonName,
   confirmButtonName,
   confirmButtonClassName = 'confirm-button',
   cancelButtonClassName = 'cancel-button',
+  options = { cancelButton: true, confirmButton: true },
 }: ModalFooterProps) {
   return (
     <div className="mt-4 flex items-center justify-end gap-2">
-      <button className={` ${cancelButtonClassName}`} onClick={() => onClose()}>
-        Cancel
-      </button>
+      {options.cancelButton && (
+        <button className={` ${cancelButtonClassName}`} onClick={() => onClose()}>
+          {cancelButtonName ? cancelButtonName : 'Cancel'}
+        </button>
+      )}
 
-      <button
-        className={` ${confirmButtonClassName}`}
-        type="submit"
-        onClick={() => onClick?.()}
-      >
-        {submitting ? <SpinnerDots /> : confirmButtonName ? confirmButtonName : 'Save'}
-      </button>
+      {options.confirmButton && (
+        <button
+          className={` ${confirmButtonClassName}`}
+          type="submit"
+          onClick={() => onClick?.()}
+        >
+          {submitting ? (
+            <SpinnerDots />
+          ) : confirmButtonName ? (
+            confirmButtonName
+          ) : (
+            'Save'
+          )}
+        </button>
+      )}
     </div>
   );
 }
