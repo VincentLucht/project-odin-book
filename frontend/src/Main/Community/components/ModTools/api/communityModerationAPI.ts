abf12dddd163502612603e0d232d0d6066249a6e
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-06-28 14:23:25 +0200

    Add making users mods

--- frontend/src/Main/Community/components/ModTools/api/communityModerationAPI.ts
+++ frontend/src/Main/Community/components/ModTools/api/communityModerationAPI.ts
@@ -3,9 +3,10 @@ import catchError from '@/util/catchError';
 import { toast } from 'react-toastify';
 import toastUpdate from '@/util/toastUpdate';
 import { APILoadingPhasesOptional } from '@/interface/misc';
-import { CommunityTypes, DBCommunity } from '@/interface/dbSchema';
+import { CommunityTypes, DBCommunity, UserRoles } from '@/interface/dbSchema';
 import { CommunityModerator } from '@/Main/Community/api/fetch/fetchCommunityWithPosts';
 import { Pagination } from '@/interface/backendTypes';
+import { MemberType } from '@/Main/Community/components/ModTools/components/Mods&Members/components/CommunityMembersApiFilters';
 
 const endpoint = '/community/mod';
 
@@ -20,6 +21,73 @@ export interface CommunityModeration extends DBCommunity {
     username: string;
   };
   community_moderators: { is_active: boolean; user: CommunityModerator }[];
+}
+
+export interface FetchedCommunityMember {
+  id: string;
+  joined_at: string;
+  role?: UserRoles;
+  created_at?: string;
+  banned_at?: string;
+  ban_reason?: string;
+  ban_duration?: string;
+  approved_at?: string;
+  is_moderator?: boolean;
+  user: {
+    username: string;
+    profile_picture_url: string | undefined;
+  };
+}
+
+export async function getMembers(
+  token: string | null,
+  communityId: string,
+  cursorId: string | undefined,
+  mode: MemberType,
+  onComplete: (members: FetchedCommunityMember[], pagination: Pagination) => void,
+) {
+  try {
+    const params = new URLSearchParams({
+      cmId: communityId,
+      cId: cursorId ?? '',
+      m: mode,
+    });
+
+    const response = await apiRequest<{
+      members: FetchedCommunityMember[];
+      pagination: Pagination;
+    }>(`/community/members?${params.toString()}`, 'GET', token);
+
+    onComplete(response.members, response.pagination);
+  } catch (error) {
+    catchError(error);
+  }
+}
+
+export async function getMembersByName(
+  token: string | null,
+  communityId: string,
+  username: string,
+  mode: MemberType,
+  onComplete: (members: FetchedCommunityMember[]) => void,
+  take?: number,
+) {
+  try {
+    const params = new URLSearchParams({
+      cmId: communityId,
+      u: username,
+      m: mode,
+      t: String(take),
+    });
+
+    const response = await apiRequest<{
+      members: FetchedCommunityMember[];
+    }>(`/community/members/search?${params.toString()}`, 'GET', token);
+
+    onComplete(response.members);
+  } catch (error) {
+    catchError(error);
+  }
 }
 
 export async function getModInfo(
@@ -83,12 +151,38 @@ export async function updateCommunitySettings(
   const toastId = toast.loading(messages?.loading);
 
   try {
-    await apiRequest(`${`${endpoint}/settings`}`, 'PUT', token, apiData);
+    await apiRequest(`${endpoint}/settings`, 'PUT', token, apiData);
     toastUpdate(toastId, 'success', messages?.success ?? 'Successfully updated');
     return true;
   } catch (error) {
     toastUpdate(toastId, 'error', messages?.error ?? 'Failed to update');
     catchError(error);
     return false;
+  }
+}
+
+export async function makeMod(
+  token: string,
+  apiData: { community_id: string; username: string },
+  onComplete: () => void,
+) {
+  try {
+    await apiRequest(`${endpoint}/user`, 'POST', token, apiData);
+    onComplete();
+  } catch (error) {
+    catchError(error);
+  }
+}
+
+export async function removeMod(
+  token: string,
+  apiData: { community_id: string; username: string },
+  onComplete: () => void,
+) {
+  try {
+    await apiRequest(`${endpoint}/user`, 'DELETE', token, apiData);
+    onComplete();
+  } catch (error) {
+    catchError(error);
   }
 }
