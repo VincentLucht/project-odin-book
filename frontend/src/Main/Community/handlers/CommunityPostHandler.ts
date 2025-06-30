882f2f8b4d0d568cf2df079db699964f483f7585
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-06-30 14:36:05 +0200

    Add saving, remove unnecessary currying

--- frontend/src/Main/Community/handlers/CommunityPostHandler.ts
+++ frontend/src/Main/Community/handlers/CommunityPostHandler.ts
@@ -3,6 +3,7 @@ import { DBPostWithCommunityName } from '@/interface/dbSchema';
 
 /**
  * Creates callback functions for Post edit API functions, uses {@link DBPostWithCommunityName} as the setter function type.
+ * Reduces code repetition across used components by reusing state code blocks.
  *
  * Uses {@link CommunityPostManager} methods.
  */
@@ -18,6 +19,7 @@ export default class CommunityPostHandler<
 > {
   private postManager: CommunityPostManager;
   private setPosts: React.Dispatch<React.SetStateAction<T[]>>;
+
   constructor(
     postManager: CommunityPostManager,
     setPosts: React.Dispatch<React.SetStateAction<T[]>>,
@@ -27,100 +29,111 @@ export default class CommunityPostHandler<
   }
 
   handleDeletePost = (postId: string) => {
-    return () => {
-      void this.postManager.deletePost(postId, (deletedPostId: string) => {
-        this.setPosts((prev) => {
-          if (!prev) return prev;
+    void this.postManager.deletePost(postId, (deletedPostId: string) => {
+      this.setPosts((prev) => {
+        if (!prev) return prev;
 
-          return prev.filter((post) => post.id !== deletedPostId);
-        });
+        return prev.filter((post) => post.id !== deletedPostId);
       });
-    };
+    });
   };
 
   handleSpoilerFunc = (post: T) => {
-    return () => {
-      this.postManager.toggleSpoiler(
-        post.id,
-        post.body,
-        post.is_spoiler,
-        post.is_mature,
-        (postId) => {
-          this.setPosts((prev) => {
-            if (!prev) return prev;
+    this.postManager.toggleSpoiler(
+      post.id,
+      post.body,
+      post.is_spoiler,
+      post.is_mature,
+      (postId) => {
+        this.setPosts((prev) => {
+          if (!prev) return prev;
 
-            return prev.map((post) => {
-              if (post.id === postId) {
-                return {
-                  ...post,
-                  is_spoiler: !post.is_spoiler,
-                  edited_at: new Date(),
-                };
-              }
+          return prev.map((post) => {
+            if (post.id === postId) {
+              return {
+                ...post,
+                is_spoiler: !post.is_spoiler,
+                edited_at: new Date(),
+              };
+            }
 
-              return post;
-            });
+            return post;
           });
-        },
-      );
-    };
+        });
+      },
+    );
   };
 
   handleMatureFunc = (post: T) => {
-    return () => {
-      this.postManager.toggleMature(
-        post.id,
-        post.body,
-        post.is_spoiler,
-        post.is_mature,
-        (postId) => {
-          this.setPosts((prev) => {
-            if (!prev) return prev;
+    this.postManager.toggleMature(
+      post.id,
+      post.body,
+      post.is_spoiler,
+      post.is_mature,
+      (postId) => {
+        this.setPosts((prev) => {
+          if (!prev) return prev;
 
-            return prev.map((post) => {
-              if (post.id === postId) {
-                return {
-                  ...post,
-                  is_mature: !post.is_mature,
-                  edited_at: new Date(),
-                };
-              }
+          return prev.map((post) => {
+            if (post.id === postId) {
+              return {
+                ...post,
+                is_mature: !post.is_mature,
+                edited_at: new Date(),
+              };
+            }
 
-              return post;
-            });
+            return post;
           });
-        },
-      );
-    };
+        });
+      },
+    );
   };
 
   handleDeletePostFlair = (post: T, navigateToEdit: () => void) => {
-    return () => {
-      if (!post.post_assigned_flair.length) {
-        navigateToEdit();
-        return;
-      }
+    if (!post.post_assigned_flair.length) {
+      navigateToEdit();
+      return;
+    }
 
-      this.postManager.deletePostFlair(
-        post.id,
-        post.post_assigned_flair[0].community_flair.id,
-        (postId) => {
-          this.setPosts((prev) => {
-            if (!prev) return prev;
+    this.postManager.deletePostFlair(
+      post.id,
+      post.post_assigned_flair[0].community_flair.id,
+      (postId) => {
+        this.setPosts((prev) => {
+          if (!prev) return prev;
 
-            return prev.map((post) => {
-              if (post.id === postId) {
-                return {
-                  ...post,
-                  post_assigned_flair: [],
-                };
-              }
+          return prev.map((post) => {
+            if (post.id === postId) {
+              return {
+                ...post,
+                post_assigned_flair: [],
+              };
+            }
 
-              return post;
-            });
+            return post;
           });
-        },
-      );
-    };
+        });
+      },
+    );
+  };
+
+  handleManageSavedPost = (postId: string, userId: string, save: boolean) => {
+    const action = save ? 'save' : 'unsave';
+
+    void this.postManager.manageSavedPost(postId, action, (postId) => {
+      this.setPosts((prev) => {
+        if (!prev) return prev;
+
+        return prev.map((post) =>
+          post.id === postId
+            ? {
+                ...post,
+                saved_by: save ? [{ user_id: userId }] : [],
+              }
+            : post,
+        );
+      });
+    });
   };
 }
