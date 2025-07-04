9cf3949cf28f1bdd7b2cde32e4e02f1c4a004a6d
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-07-05 01:37:48 +0200

    Add amount prop

--- frontend/src/Main/Pages/Homepage/components/HomepageLazy.tsx
+++ frontend/src/Main/Pages/Homepage/components/HomepageLazy.tsx
@@ -2,7 +2,8 @@ import LazyPostOverviews from '@/components/Lazy/LazyPostOverviews';
 
 interface HomepageLazyProps {
   showSidebar: boolean;
+  amount?: number;
 }
-export default function HomepageLazy({ showSidebar }: HomepageLazyProps) {
-  return <LazyPostOverviews amount={6} showSidebar={showSidebar} />;
+export default function HomepageLazy({ showSidebar, amount = 6 }: HomepageLazyProps) {
+  return <LazyPostOverviews amount={amount} showSidebar={showSidebar} />;
 }
