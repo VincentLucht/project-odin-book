5f2cfec734162967bfff03372372b71edf6d4469
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-07-05 00:49:13 +0200

    Change function to update existing toast instead of blocking new creation

--- frontend/src/util/notLoggedInError.ts
+++ frontend/src/util/notLoggedInError.ts
@@ -1,5 +1,16 @@
 import { toast } from 'react-toastify';
 
 export default function notLoggedInError(message = 'Please log in to continue') {
-  toast.isActive('not-logged-in') || toast.error(message, { toastId: 'not-logged-in' });
+  const toastId = 'not-logged-in';
+
+  if (toast.isActive(toastId)) {
+    toast.update(toastId, {
+      render: message,
+      type: 'error',
+      isLoading: false,
+      autoClose: 5000,
+    });
+  } else {
+    toast.error(message, { toastId });
+  }
 }
