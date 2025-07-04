c9c0f716f21471c0ab50d2d8dc20b769cf970a6e
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-07-05 00:23:19 +0200

    Improve message for no joined communities

--- frontend/src/SideBar/components/JoinedCommunities/JoinedCommunities.tsx
+++ frontend/src/SideBar/components/JoinedCommunities/JoinedCommunities.tsx
@@ -75,8 +75,7 @@ export default function JoinedCommunities({
           />
         ))}
         {joinedCommunities.length === 0 ? (
-          // TODO: Add better?
-          <div className="px-4 py-2 df">Join a community!</div>
+          <div className="max-w-[215px] px-4 py-2 df">No joined communities yet</div>
         ) : (
           hasMore && (
             <SidebarButton
