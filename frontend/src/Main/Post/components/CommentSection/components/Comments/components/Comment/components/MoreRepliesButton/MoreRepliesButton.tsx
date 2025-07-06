1d71756ea00afbee93bdf1f1380b51cde39e7dc3
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-07-06 11:57:26 +0200

    Fix navigation breaking after first comment nest
    - also fix btn stretching

--- frontend/src/Main/Post/components/CommentSection/components/Comments/components/Comment/components/MoreRepliesButton/MoreRepliesButton.tsx
+++ frontend/src/Main/Post/components/CommentSection/components/Comments/components/Comment/components/MoreRepliesButton/MoreRepliesButton.tsx
@@ -7,7 +7,6 @@ interface MoreRepliesProps {
   parent_comment_id: string;
 }
 
-// TODO: Fix the "More replies" button stretching out
 export default function MoreRepliesButton({
   hasReplyAtAll,
   parent_comment_id,
@@ -19,15 +18,24 @@ export default function MoreRepliesButton({
     return;
   }
 
+  const handleNavigate = () => {
+    const pathParts = location.pathname.split('/').filter(Boolean);
+
+    // Only the first 4 parts and append the new parent_comment_id
+    // ? r/community_name/post_id/post-title(slugged)
+    const basePath = pathParts.slice(0, 4).join('/');
+    const newPath = `/${basePath}/${parent_comment_id}`;
+
+    navigate(newPath);
+  };
+
   return (
     <div
-      className="group relative ml-[40px] flex cursor-pointer items-center gap-[2px]"
-      onClick={() => navigate(`${location.pathname}/${parent_comment_id}`)}
+      className="group relative ml-[40px] flex w-fit cursor-pointer items-center gap-[2px]"
+      onClick={handleNavigate}
     >
       <div className="comment-connector-line-more-replies"></div>
-
       <CirclePlusIcon className="h-[18px] transition-all group-hover:scale-110 group-active:scale-95" />
-
       <div className="text-sm text-gray-500 group-hover:underline">More replies</div>
     </div>
   );
