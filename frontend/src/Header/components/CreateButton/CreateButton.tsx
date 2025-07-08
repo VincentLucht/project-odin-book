d77b0fdd1cfc59cd2d74b923f143f8c74ecb5709
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-07-08 15:37:21 +0200

    Add props instead calling hook

--- frontend/src/Header/components/CreateButton/CreateButton.tsx
+++ frontend/src/Header/components/CreateButton/CreateButton.tsx
@@ -1,16 +1,18 @@
-import { useLocation, useNavigate } from 'react-router-dom';
+import { Location, NavigateFunction } from 'react-router-dom';
 
 import { PlusIcon } from 'lucide-react';
 
 import handleCreatePostClick from '@/Header/components/CreateButton/util/handleCreatePostClick';
 
-export default function CreateButton() {
-  const navigate = useNavigate();
-  const location = useLocation();
+interface CreateButtonProps {
+  location: Location<any>;
+  navigate: NavigateFunction;
+}
 
+export default function CreateButton({ location, navigate }: CreateButtonProps) {
   return (
     <div
-      className="flex-shrink-0 df bg-hover-transition"
+      className="flex-shrink-0 bg-hover-transition"
       onClick={() => handleCreatePostClick(location.pathname, navigate)}
     >
       <PlusIcon className="h-8 w-8" />
