dbec896030162987036b87d7e8b6a78b22e68c33
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-07-09 15:48:37 +0200

    Allow frontend to interact with API

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,12 +8,15 @@ const app = express();
 // Configure CORS globally
 app.use(
   cors({
-    origin: '*', // ! allow for portfolio showcase
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true, // ! allow for portfolio showcase
+    origin: [
+      'https://project-odin-book-mocha.vercel.app',
+      'http://localhost:3000',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
   }),
 );
-
 app.use(express.json()); // ? parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // ? allow req.body
 
