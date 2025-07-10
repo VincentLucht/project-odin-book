1ee3db259142d08f0dbf57bb1bf0269a8f71ef38
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2025-07-10 02:15:18 +0200

    Try fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -37,7 +37,7 @@ app.get('/health', (req, res) => {
 
 app.use('/', router);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 3000;
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Listening on Port ${PORT}`);
 });
